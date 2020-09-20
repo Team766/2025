d10400320ae4a9344249b4ba648a91f3416be0e0
Ryan Cahoon <ryan.m.cahoon@gmail.com>
2020-09-20 20:31:19 +0000

    Drive Straight tutorial code

---
+++ src/main/java/com/team766/robot/tutorial/OI.java
@@ -0,0 +1,27 @@
+package com.team766.robot.tutorial;
+
+import com.team766.framework.RuleEngine;
+import com.team766.hal.JoystickReader;
+import com.team766.hal.RobotProvider;
+import com.team766.logging.Category;
+import com.team766.robot.tutorial.mechanisms.*;
+import com.team766.robot.tutorial.procedures.*;
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the code that allow control of the robot.
+ */
+public class OI extends RuleEngine {
+    public OI() {
+        final JoystickReader joystick0 = RobotProvider.instance.getJoystick(0);
+        final JoystickReader joystick1 = RobotProvider.instance.getJoystick(1);
+        final JoystickReader joystick2 = RobotProvider.instance.getJoystick(2);
+
+        // Add driver control rules here.
+    }
+
+    @Override
+    public Category getLoggerCategory() {
+        return Category.OPERATOR_INTERFACE;
+    }
+}
---
+++ src/main/java/com/team766/robot/tutorial/Robot.java
@@ -0,0 +1,43 @@
+package com.team766.robot.tutorial;
+
+import com.team766.framework.AutonomousMode;
+import com.team766.framework.RuleEngine;
+import com.team766.hal.RobotConfigurator;
+import com.team766.robot.tutorial.mechanisms.*;
+import com.team766.robot.tutorial.procedures.*;
+
+public class Robot implements RobotConfigurator {
+    private Drive drive;
+
+    @Override
+    public void initializeMechanisms() {
+        // Initialize mechanisms here
+        drive = new Drive();
+    }
+
+    @Override
+    public RuleEngine createOI() {
+        return new OI();
+    }
+
+    @Override
+    public RuleEngine createLights() {
+        return new Lights();
+    }
+
+    @Override
+    public AutonomousMode[] getAutonomousModes() {
+        return new AutonomousMode[] {
+            // Add autonomous modes here like this:
+            //    new AutonomousMode("NameOfAutonomousMode", () -> new MyAutonomousProcedure()),
+            //
+            // If your autonomous procedure has constructor arguments, you can
+            // define one or more different autonomous modes with it like this:
+            //    new AutonomousMode("DriveFast", () -> new DriveStraight(1.0)),
+            //    new AutonomousMode("DriveSlow", () -> new DriveStraight(0.4)),
+
+            new AutonomousMode("DriveStraight", () -> new DriveStraight(drive)),
+            new AutonomousMode("DoNothing", () -> new DoNothing()),
+        };
+    }
+}
