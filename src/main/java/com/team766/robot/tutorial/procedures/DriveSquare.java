274d509ace968487419f0afeb22c70c577b111a3
Ryan Cahoon <ryan.m.cahoon@gmail.com>
2020-09-21 17:25:01 +0000

    Add logging to Drive Square tutorial code

--- src/main/java/com/team766/robot/tutorial/procedures/DriveSquare.java
+++ src/main/java/com/team766/robot/tutorial/procedures/DriveSquare.java
@@ -2,6 +2,7 @@ package com.team766.robot.tutorial.procedures;
 
 import com.team766.framework.Context;
 import com.team766.framework.Procedure;
+import com.team766.logging.Category;
 import com.team766.robot.tutorial.mechanisms.Drive;
 
 public class DriveSquare extends Procedure {
@@ -15,26 +16,39 @@ public class DriveSquare extends Procedure {
     public void run(Context context) {
         // First side
         context.runSync(new DriveStraight(drive));
+        log("First side complete");
 
         // First corner
         context.runSync(new TurnRight(drive));
+        log("First corner complete");
 
         // Second side
         context.runSync(new DriveStraight(drive));
+        log("Second side complete");
 
         // Second corner
         context.runSync(new TurnRight(drive));
+        log("Second corner complete");
 
         // Third side
         context.runSync(new DriveStraight(drive));
+        log("Third side complete");
 
         // Third corner
         context.runSync(new TurnRight(drive));
+        log("Third corner complete");
 
         // Fourth side
         context.runSync(new DriveStraight(drive));
+        log("Fourth side complete");
 
         // Fourth corner
         context.runSync(new TurnRight(drive));
+        log("Fourth corner complete");
+    }
+
+    @Override
+    public Category getLoggerCategory() {
+        return Category.AUTONOMOUS;
     }
 }
