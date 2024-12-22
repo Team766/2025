Team766/2024

Migrate robot code

src/main/java/com/team766/robot/reva/procedures/MidfieldAutonSourceSide.java
@@ -1,17 +1,35 @@
 package com.team766.robot.reva.procedures;
 
-import com.team766.robot.common.procedures.PathSequenceAuto;
-import com.team766.robot.reva.Robot;
+import com.team766.framework3.Context;
+import com.team766.robot.common.mechanisms.SwerveDrive;
+import com.team766.robot.reva.mechanisms.Intake;
+import com.team766.robot.reva.mechanisms.Shooter;
+import com.team766.robot.reva.mechanisms.Superstructure;
+import com.team766.robot.reva.procedures.auton_routines.AutoBase;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 
-public class MidfieldAutonSourceSide extends PathSequenceAuto {
-    public MidfieldAutonSourceSide() {
-        super(Robot.drive, new Pose2d(0.71, 4.39, Rotation2d.fromDegrees(-60)));
-        addProcedure(new ShootNow());
-        addProcedure(new StartAutoIntake());
-        addPath("MidfieldSource 1");
-        addPath("MidfieldSource 2");
-        addProcedure(new ShootNow());
+public class MidfieldAutonSourceSide extends AutoBase {
+    private final SwerveDrive drive;
+    private final Superstructure superstructure;
+    private final Shooter shooter;
+    private final Intake intake;
+
+    public MidfieldAutonSourceSide(
+            SwerveDrive drive, Superstructure superstructure, Shooter shooter, Intake intake) {
+        super(drive, superstructure, shooter, new Pose2d(0.71, 4.39, Rotation2d.fromDegrees(-60)));
+        this.drive = reserve(drive);
+        this.superstructure = reserve(superstructure);
+        this.shooter = reserve(shooter);
+        this.intake = reserve(intake);
+    }
+
+    @Override
+    protected void runAuto(Context context) {
+        context.runSync(new ShootNow(drive, superstructure, shooter, intake));
+        context.runSync(new StartAutoIntake(superstructure, intake));
+        runPath(context, "MidfieldSource 1");
+        runPath(context, "MidfieldSource 2");
+        context.runSync(new ShootNow(drive, superstructure, shooter, intake));
     }
 }

src/main/java/com/team766/robot/reva/procedures/ShootAtSubwoofer.java
@@ -1,15 +1,25 @@
 package com.team766.robot.reva.procedures;
 
-import com.team766.framework.Context;
-import com.team766.framework.Procedure;
-import com.team766.robot.reva.Robot;
-import com.team766.robot.reva.mechanisms.Shoulder.ShoulderPosition;
+import com.team766.framework3.Context;
+import com.team766.framework3.Procedure;
+import com.team766.robot.reva.mechanisms.Intake;
+import com.team766.robot.reva.mechanisms.Shooter;
+import com.team766.robot.reva.mechanisms.Shoulder;
+import com.team766.robot.reva.mechanisms.Superstructure;
 
 public class ShootAtSubwoofer extends Procedure {
+    private final Superstructure superstructure;
+    private final Shooter shooter;
+    private final Intake intake;
+
+    public ShootAtSubwoofer(Superstructure superstructure, Shooter shooter, Intake intake) {
+        this.superstructure = reserve(superstructure);
+        this.shooter = reserve(shooter);
+        this.intake = reserve(intake);
+    }
+
     public void run(Context context) {
-        context.takeOwnership(Robot.shoulder);
-        Robot.shoulder.rotate(ShoulderPosition.SHOOT_LOW);
-        context.releaseOwnership(Robot.shoulder);
-        context.runSync(new ShootVelocityAndIntake());
+        superstructure.setRequest(Shoulder.RotateToPosition.SHOOT_LOW);
+        context.runSync(new ShootVelocityAndIntake(shooter, intake));
     }
 }

src/main/java/com/team766/robot/reva/procedures/auton_routines/AutoBase.java
@@ -1,0 +1,37 @@
+package com.team766.robot.reva.procedures.auton_routines;
+
+import com.team766.framework3.Context;
+import com.team766.robot.common.mechanisms.SwerveDrive;
+import com.team766.robot.common.procedures.PathSequenceAuto;
+import com.team766.robot.reva.mechanisms.Climber;
+import com.team766.robot.reva.mechanisms.Shooter;
+import com.team766.robot.reva.mechanisms.Superstructure;
+import edu.wpi.first.math.geometry.Pose2d;
+
+public abstract class AutoBase extends PathSequenceAuto {
+
+    private final Shooter shooter;
+    private final Superstructure superstructure;
+
+    public AutoBase(
+            SwerveDrive drive,
+            Superstructure superstructure,
+            Shooter shooter,
+            Pose2d initialPosition) {
+        super(drive, initialPosition);
+        this.shooter = reserve(shooter);
+        this.superstructure = reserve(superstructure);
+    }
+
+    protected abstract void runAuto(Context context);
+
+    @Override
+    protected final void runSequence(Context context) {
+        superstructure.setRequest(Climber.MoveToPosition.BOTTOM);
+        try {
+            runAuto(context);
+        } finally {
+            shooter.setRequest(new Shooter.Stop());
+        }
+    }
+}

src/main/java/com/team766/robot/reva/procedures/auton_routines/JustShootAmp.java
@@ -1,14 +1,31 @@
 package com.team766.robot.reva.procedures.auton_routines;
 
+import com.team766.framework3.Context;
+import com.team766.robot.common.mechanisms.SwerveDrive;
 import com.team766.robot.common.procedures.PathSequenceAuto;
-import com.team766.robot.reva.Robot;
+import com.team766.robot.reva.mechanisms.Intake;
+import com.team766.robot.reva.mechanisms.Shooter;
+import com.team766.robot.reva.mechanisms.Superstructure;
 import com.team766.robot.reva.procedures.ShootAtSubwoofer;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 
 public class JustShootAmp extends PathSequenceAuto {
-    public JustShootAmp() {
-        super(Robot.drive, new Pose2d(0.71, 6.72, Rotation2d.fromDegrees(60)));
-        addProcedure(new ShootAtSubwoofer());
+
+    private final Superstructure superstructure;
+    private final Shooter shooter;
+    private final Intake intake;
+
+    public JustShootAmp(
+            SwerveDrive drive, Superstructure superstructure, Shooter shooter, Intake intake) {
+        super(drive, new Pose2d(0.71, 6.72, Rotation2d.fromDegrees(60)));
+        this.superstructure = superstructure;
+        this.shooter = shooter;
+        this.intake = intake;
+    }
+
+    @Override
+    protected void runSequence(Context context) {
+        context.runSync(new ShootAtSubwoofer(superstructure, shooter, intake));
     }
 }

src/main/java/com/team766/robot/reva/procedures/auton_routines/ThreePieceMidfieldAmpSide.java
@@ -1,22 +1,39 @@
 package com.team766.robot.reva.procedures.auton_routines;
 
-import com.team766.robot.common.procedures.PathSequenceAuto;
-import com.team766.robot.reva.Robot;
+import com.team766.framework3.Context;
+import com.team766.robot.common.mechanisms.SwerveDrive;
+import com.team766.robot.reva.mechanisms.Intake;
+import com.team766.robot.reva.mechanisms.Shooter;
+import com.team766.robot.reva.mechanisms.Superstructure;
 import com.team766.robot.reva.procedures.ShootAtSubwoofer;
 import com.team766.robot.reva.procedures.ShootNow;
 import com.team766.robot.reva.procedures.StartAutoIntake;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 
-public class ThreePieceMidfieldAmpSide extends PathSequenceAuto {
-    public ThreePieceMidfieldAmpSide() {
-        super(Robot.drive, new Pose2d(0.71, 6.72, Rotation2d.fromDegrees(60)));
-        addProcedure(new ShootAtSubwoofer());
-        addProcedure(new StartAutoIntake());
-        addPath("Amp Side Start to Top Piece");
-        addProcedure(new ShootNow());
-        addProcedure(new StartAutoIntake());
-        addPath("Retrieve Top Midfield from Top Piece");
-        // addProcedure(new ShootNow());
+public class ThreePieceMidfieldAmpSide extends AutoBase {
+    private final SwerveDrive drive;
+    private final Superstructure superstructure;
+    private final Shooter shooter;
+    private final Intake intake;
+
+    public ThreePieceMidfieldAmpSide(
+            SwerveDrive drive, Superstructure superstructure, Shooter shooter, Intake intake) {
+        super(drive, superstructure, shooter, new Pose2d(0.71, 6.72, Rotation2d.fromDegrees(60)));
+        this.drive = reserve(drive);
+        this.superstructure = reserve(superstructure);
+        this.shooter = reserve(shooter);
+        this.intake = reserve(intake);
+    }
+
+    @Override
+    protected void runAuto(Context context) {
+        context.runSync(new ShootAtSubwoofer(superstructure, shooter, intake));
+        context.runSync(new StartAutoIntake(superstructure, intake));
+        runPath(context, "Amp Side Start to Top Piece");
+        context.runSync(new ShootNow(drive, superstructure, shooter, intake));
+        context.runSync(new StartAutoIntake(superstructure, intake));
+        runPath(context, "Retrieve Top Midfield from Top Piece");
+        // context.runSync(new ShootNow(drive, superstructure, shooter, intake));
     }
 }

Team766/2025

src/main/java/com/team766/robot/reva/procedures/MidfieldAutonSourceSide.java
@@ -1,17 +1,35 @@
 package com.team766.robot.reva.procedures;
 
-import com.team766.robot.common.procedures.PathSequenceAuto;
-import com.team766.robot.reva.Robot;
+import com.team766.framework3.Context;
+import com.team766.robot.common.mechanisms.SwerveDrive;
+import com.team766.robot.reva.mechanisms.ArmAndClimber;
+import com.team766.robot.reva.mechanisms.Intake;
+import com.team766.robot.reva.mechanisms.Shooter;
+import com.team766.robot.reva.procedures.auton_routines.AutoBase;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 
-public class MidfieldAutonSourceSide extends PathSequenceAuto {
-    public MidfieldAutonSourceSide() {
-        super(Robot.drive, new Pose2d(0.71, 4.39, Rotation2d.fromDegrees(-60)));
-        addProcedure(new ShootNow());
-        addProcedure(new StartAutoIntake());
-        addPath("MidfieldSource 1");
-        addPath("MidfieldSource 2");
-        addProcedure(new ShootNow());
+public class MidfieldAutonSourceSide extends AutoBase {
+    private final SwerveDrive drive;
+    private final ArmAndClimber superstructure;
+    private final Shooter shooter;
+    private final Intake intake;
+
+    public MidfieldAutonSourceSide(
+            SwerveDrive drive, ArmAndClimber superstructure, Shooter shooter, Intake intake) {
+        super(drive, superstructure, shooter, new Pose2d(0.71, 4.39, Rotation2d.fromDegrees(-60)));
+        this.drive = reserve(drive);
+        this.superstructure = reserve(superstructure);
+        this.shooter = reserve(shooter);
+        this.intake = reserve(intake);
+    }
+
+    @Override
+    protected void runAuto(Context context) {
+        context.runSync(new ShootNow(drive, superstructure, shooter, intake));
+        context.runSync(new StartAutoIntake(superstructure, intake));
+        runPath(context, "MidfieldSource 1");
+        runPath(context, "MidfieldSource 2");
+        context.runSync(new ShootNow(drive, superstructure, shooter, intake));
     }
 }

src/main/java/com/team766/robot/reva/procedures/ShootAtSubwoofer.java
@@ -1,15 +1,25 @@
 package com.team766.robot.reva.procedures;
 
-import com.team766.framework.Context;
-import com.team766.framework.Procedure;
-import com.team766.robot.reva.Robot;
-import com.team766.robot.reva.mechanisms.Shoulder.ShoulderPosition;
+import com.team766.framework3.Context;
+import com.team766.framework3.Procedure;
+import com.team766.robot.reva.mechanisms.ArmAndClimber;
+import com.team766.robot.reva.mechanisms.Intake;
+import com.team766.robot.reva.mechanisms.Shooter;
+import com.team766.robot.reva.mechanisms.Shoulder;
 
 public class ShootAtSubwoofer extends Procedure {
+    private final ArmAndClimber superstructure;
+    private final Shooter shooter;
+    private final Intake intake;
+
+    public ShootAtSubwoofer(ArmAndClimber superstructure, Shooter shooter, Intake intake) {
+        this.superstructure = reserve(superstructure);
+        this.shooter = reserve(shooter);
+        this.intake = reserve(intake);
+    }
+
     public void run(Context context) {
-        context.takeOwnership(Robot.shoulder);
-        Robot.shoulder.rotate(ShoulderPosition.SHOOT_LOW);
-        context.releaseOwnership(Robot.shoulder);
-        context.runSync(new ShootVelocityAndIntake());
+        superstructure.setRequest(Shoulder.RotateToPosition.SHOOT_LOW);
+        context.runSync(new ShootVelocityAndIntake(shooter, intake));
     }
 }

src/main/java/com/team766/robot/reva/procedures/auton_routines/AutoBase.java
@@ -1,0 +1,37 @@
+package com.team766.robot.reva.procedures.auton_routines;
+
+import com.team766.framework3.Context;
+import com.team766.robot.common.mechanisms.SwerveDrive;
+import com.team766.robot.common.procedures.PathSequenceAuto;
+import com.team766.robot.reva.mechanisms.ArmAndClimber;
+import com.team766.robot.reva.mechanisms.Climber;
+import com.team766.robot.reva.mechanisms.Shooter;
+import edu.wpi.first.math.geometry.Pose2d;
+
+public abstract class AutoBase extends PathSequenceAuto {
+
+    private final Shooter shooter;
+    private final ArmAndClimber superstructure;
+
+    public AutoBase(
+            SwerveDrive drive,
+            ArmAndClimber superstructure,
+            Shooter shooter,
+            Pose2d initialPosition) {
+        super(drive, initialPosition);
+        this.shooter = reserve(shooter);
+        this.superstructure = reserve(superstructure);
+    }
+
+    protected abstract void runAuto(Context context);
+
+    @Override
+    protected final void runSequence(Context context) {
+        superstructure.setRequest(Climber.MoveToPosition.BOTTOM);
+        try {
+            runAuto(context);
+        } finally {
+            shooter.setRequest(new Shooter.Stop());
+        }
+    }
+}

src/main/java/com/team766/robot/reva/procedures/auton_routines/JustShootAmp.java
@@ -1,14 +1,31 @@
 package com.team766.robot.reva.procedures.auton_routines;
 
+import com.team766.framework3.Context;
+import com.team766.robot.common.mechanisms.SwerveDrive;
 import com.team766.robot.common.procedures.PathSequenceAuto;
-import com.team766.robot.reva.Robot;
+import com.team766.robot.reva.mechanisms.ArmAndClimber;
+import com.team766.robot.reva.mechanisms.Intake;
+import com.team766.robot.reva.mechanisms.Shooter;
 import com.team766.robot.reva.procedures.ShootAtSubwoofer;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 
 public class JustShootAmp extends PathSequenceAuto {
-    public JustShootAmp() {
-        super(Robot.drive, new Pose2d(0.71, 6.72, Rotation2d.fromDegrees(60)));
-        addProcedure(new ShootAtSubwoofer());
+
+    private final ArmAndClimber superstructure;
+    private final Shooter shooter;
+    private final Intake intake;
+
+    public JustShootAmp(
+            SwerveDrive drive, ArmAndClimber superstructure, Shooter shooter, Intake intake) {
+        super(drive, new Pose2d(0.71, 6.72, Rotation2d.fromDegrees(60)));
+        this.superstructure = superstructure;
+        this.shooter = shooter;
+        this.intake = intake;
+    }
+
+    @Override
+    protected void runSequence(Context context) {
+        context.runSync(new ShootAtSubwoofer(superstructure, shooter, intake));
     }
 }

src/main/java/com/team766/robot/reva/procedures/auton_routines/ThreePieceMidfieldAmpSide.java
@@ -1,22 +1,39 @@
 package com.team766.robot.reva.procedures.auton_routines;
 
-import com.team766.robot.common.procedures.PathSequenceAuto;
-import com.team766.robot.reva.Robot;
+import com.team766.framework3.Context;
+import com.team766.robot.common.mechanisms.SwerveDrive;
+import com.team766.robot.reva.mechanisms.ArmAndClimber;
+import com.team766.robot.reva.mechanisms.Intake;
+import com.team766.robot.reva.mechanisms.Shooter;
 import com.team766.robot.reva.procedures.ShootAtSubwoofer;
 import com.team766.robot.reva.procedures.ShootNow;
 import com.team766.robot.reva.procedures.StartAutoIntake;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 
-public class ThreePieceMidfieldAmpSide extends PathSequenceAuto {
-    public ThreePieceMidfieldAmpSide() {
-        super(Robot.drive, new Pose2d(0.71, 6.72, Rotation2d.fromDegrees(60)));
-        addProcedure(new ShootAtSubwoofer());
-        addProcedure(new StartAutoIntake());
-        addPath("Amp Side Start to Top Piece");
-        addProcedure(new ShootNow());
-        addProcedure(new StartAutoIntake());
-        addPath("Retrieve Top Midfield from Top Piece");
-        // addProcedure(new ShootNow());
+public class ThreePieceMidfieldAmpSide extends AutoBase {
+    private final SwerveDrive drive;
+    private final ArmAndClimber superstructure;
+    private final Shooter shooter;
+    private final Intake intake;
+
+    public ThreePieceMidfieldAmpSide(
+            SwerveDrive drive, ArmAndClimber superstructure, Shooter shooter, Intake intake) {
+        super(drive, superstructure, shooter, new Pose2d(0.71, 6.72, Rotation2d.fromDegrees(60)));
+        this.drive = reserve(drive);
+        this.superstructure = reserve(superstructure);
+        this.shooter = reserve(shooter);
+        this.intake = reserve(intake);
+    }
+
+    @Override
+    protected void runAuto(Context context) {
+        context.runSync(new ShootAtSubwoofer(superstructure, shooter, intake));
+        context.runSync(new StartAutoIntake(superstructure, intake));
+        runPath(context, "Amp Side Start to Top Piece");
+        context.runSync(new ShootNow(drive, superstructure, shooter, intake));
+        context.runSync(new StartAutoIntake(superstructure, intake));
+        runPath(context, "Retrieve Top Midfield from Top Piece");
+        // context.runSync(new ShootNow(drive, superstructure, shooter, intake));
     }
 }