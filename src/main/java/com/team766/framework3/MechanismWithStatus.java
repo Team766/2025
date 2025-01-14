ae0baa0c2709196710ddabab9dc699e91bcd1a80
Ryan Cahoon <ryan.m.cahoon@gmail.com>
2025-01-13 22:42:04 -0800

    Resolve merge

--- src/main/java/com/team766/framework3/MechanismWithStatus.java
+++ src/main/java/com/team766/framework3/MechanismWithStatus.java
@@ -1,8 +1,17 @@
 package com.team766.framework3;
 
+import java.util.NoSuchElementException;
+
 public abstract class MechanismWithStatus<S extends Record & Status> extends Mechanism
         implements StatusSource<S> {
     private S status = null;
+
+    protected final S getStatus() {
+        if (status == null) {
+            throw new NoSuchElementException(getName() + " has not published a status yet");
+        }
+        return status;
+    }
 
     @Override
     /* package */ final void publishStatus() {
