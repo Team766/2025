3e80fb354060a06139b49930902ec836f46fd2df
Ravioli1976 <ditya.kamath@gmail.com>
2025-01-15 19:08:43 -0800

    added wrist code (#3)
    ## Description
    
    Please include a summary of the changes in this PR. If it is fixing an
    issue, describe the issue. Please also include relevant motivation and
    context such that a newer programmer could understand your changes just
    by reading this description.
    
    Code for the 360 degree motor controlling the arm used for intaking and
    placing coral.
    
    
    ## How Has This Been Tested?
    
    Please describe the tests that you ran to verify your changes. Be
    detailed so that your code reviewer can understand exactly how much and
    what kinds of testing were done, and which might still be worthwhile to
    do.
    
    Untested (For Now)
    
    - [ ] Unit tests: [Add your description here]
    - [ ] Simulator testing: [Add your description here]
    - [ ] On-robot bench testing: [Add your description here]
    - [ ] On-robot field testing: [Add your description here]
    
    ---------
    
    Co-authored-by: DS <team766robotics@gmail.com>
    Co-authored-by: Max Spier <68516760+maxspier@users.noreply.github.com>

---
+++ src/main/java/com/team766/robot/reva_2025/mechanisms/Wrist.java
@@ -0,0 +1,38 @@
+import com.team766.framework.Mechanism;
+import com.team766.hal.MotorController;
+import com.team766.hal.RobotProvider;
+
+public class Wrist extends Mechanism {
+
+    public enum WristPosition {
+
+        // TODO: update all of these to real things!
+        PICKUP_CORAL(0),
+        PLACE_CORAL(180);
+
+        private final double angle;
+
+        private WristPosition(double angle) {
+            this.angle = angle;
+        }
+
+        public double getAngle() {
+            return angle;
+        }
+    }
+
+    private MotorController wristMotor;
+
+    public Wrist() {
+        wristMotor = RobotProvider.instance.getMotor("Wrist.Motor");
+    }
+
+    public void setAngle(WristPosition position) {
+        setAngle(position.getAngle());
+    }
+
+    public void setAngle(double angle) {
+        checkContextOwnership();
+        wristMotor.set(MotorController.ControlMode.Position, angle);
+    }
+}
