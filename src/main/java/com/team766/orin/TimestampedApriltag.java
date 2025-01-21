9b0e193971deaf8421b341d872d8893d6d2403ea
Ryan Cahoon <ryan.m.cahoon@gmail.com>
2025-01-21 14:53:38 -0800

    Convert TimestampedApriltag to a record type for better logging

--- src/main/java/com/team766/orin/TimestampedApriltag.java
+++ src/main/java/com/team766/orin/TimestampedApriltag.java
@@ -1,26 +1,14 @@
 package com.team766.orin;
 
 import com.team766.robot.reva_2025.constants.FieldConstants;
-import edu.wpi.first.apriltag.AprilTag;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 
-public class TimestampedApriltag extends AprilTag {
-    private final double collectTime;
-
-    public TimestampedApriltag(double collectTime, int tagId, Pose3d pose) {
-        super(tagId, pose);
-        this.collectTime = collectTime;
-    }
-
-    public double getCollectTime() {
-        return collectTime;
-    }
-
+public record TimestampedApriltag(double collectTime, int tagId, Pose3d pose) {
     public Translation2d toRobotPosition(Rotation2d gyro) {
         return FieldConstants.APRIL_TAG_REEFSCAPE_LAYOUT
-                .getTagPose(this.ID)
+                .getTagPose(this.tagId())
                 .get()
                 .getTranslation()
                 .toTranslation2d()
--- src/main/java/com/team766/robot/common/mechanisms/SwerveDrive.java
+++ src/main/java/com/team766/robot/common/mechanisms/SwerveDrive.java
@@ -372,7 +372,7 @@ public class SwerveDrive extends MechanismWithStatus<SwerveDrive.DriveStatus> {
                         tagPoses.add(tag.toRobotPosition(Rotation2d.fromDegrees(heading)));
                     }
                     kalmanFilter.updateWithVisionMeasurement(
-                            tagPoses, cameraTags.get(0).getCollectTime());
+                            tagPoses, cameraTags.get(0).collectTime());
                 }
             }
         }
