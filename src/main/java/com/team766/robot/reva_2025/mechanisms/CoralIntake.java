130ce575d7c0fe1d022144463ee157132e302ab0
xavier <avier@gmail.com>
2025-01-25 15:45:55 -0800

    added mechanisms with status to coralintake and elevator

--- src/main/java/com/team766/robot/reva_2025/mechanisms/CoralIntake.java
+++ src/main/java/com/team766/robot/reva_2025/mechanisms/CoralIntake.java
@@ -1,32 +1,40 @@
 package com.team766.robot.reva_2025.mechanisms;
 
-import com.team766.framework.Mechanism;
+import com.team766.framework3.MechanismWithStatus;
+import com.team766.framework3.Status;
 import com.team766.hal.MotorController;
 import com.team766.hal.RobotProvider;
 
-public class CoralIntake extends Mechanism {
+public class CoralIntake extends MechanismWithStatus<CoralIntake.CoralIntakeStatus> {
 
     private static final double POWER_IN = 1.0;
     private static final double POWER_OUT = -1.0;
-
+    private double currentPower;
     private MotorController motor;
+
+    public static record CoralIntakeStatus(double currentPower) implements Status {}
 
     public CoralIntake() {
         motor = RobotProvider.instance.getMotor("coralIntake.motor");
+        currentPower = 0;
     }
 
     public void in() {
-        checkContextOwnership();
         motor.set(POWER_IN);
+        currentPower = POWER_IN;
     }
 
     public void out() {
-        checkContextOwnership();
         motor.set(POWER_OUT);
+        currentPower = POWER_OUT;
     }
 
     public void stop() {
-        checkContextOwnership();
         motor.set(0.0);
+        currentPower = 0;
+    }
+
+    protected CoralIntakeStatus updateStatus() {
+        return new CoralIntakeStatus(currentPower);
     }
 }
--- src/main/java/com/team766/robot/reva_2025/mechanisms/Elevator.java
+++ src/main/java/com/team766/robot/reva_2025/mechanisms/Elevator.java
@@ -1,41 +1,49 @@
 package com.team766.robot.reva_2025.mechanisms;
 
-import com.team766.framework.Mechanism;
+import com.team766.framework3.MechanismWithStatus;
+import com.team766.framework3.Status;
 import com.team766.hal.MotorController;
 import com.team766.hal.RobotProvider;
 
-public class Elevator extends Mechanism {
+public class Elevator extends MechanismWithStatus<Elevator.ElevatorStatus> {
     private MotorController elevatorLeftMotor;
     private MotorController elevatorRightMotor;
+    private double currentPosition;
     private final double MIN_HEIGHT = 0;
     private final double MAX_HEIGHT = 150;
     private final double NUDGE_AMOUNT = 5;
 
-    // values are untested and are set to change
+    // values are untested and are set to 
+
+    public static record ElevatorStatus(double currentPosition) implements Status {}
 
     public Elevator() {
         elevatorLeftMotor = RobotProvider.instance.getMotor("elevator.leftMotor");
         elevatorRightMotor = RobotProvider.instance.getMotor("elevator.rightMotor");
         elevatorRightMotor.follow(elevatorLeftMotor);
+        currentPosition = 0;
         elevatorLeftMotor.setSensorPosition(0);
     }
 
     public void setPosition(double setPosition) {
-        checkContextOwnership();
         if (setPosition >= MIN_HEIGHT && setPosition <= MAX_HEIGHT) {
             elevatorLeftMotor.set(MotorController.ControlMode.Position, setPosition);
+            currentPosition = setPosition;
         }
     }
 
     public void nudgeUp() {
-        checkContextOwnership();
         double nudgePosition = elevatorLeftMotor.getSensorPosition() + NUDGE_AMOUNT;
         setPosition(nudgePosition);
     }
 
     public void nudgeDown() {
-        checkContextOwnership();
         double nudgePosition = elevatorLeftMotor.getSensorPosition() - NUDGE_AMOUNT;
         setPosition(nudgePosition);
+
+    }
+
+    protected ElevatorStatus updateStatus() {
+        return new ElevatorStatus(currentPosition);
     }
 }
