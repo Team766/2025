db72927b1f24135b5b1679fc43b1b378c2b1e6c0
xavier <avier@gmail.com>
2025-01-24 19:27:31 -0800

    Added strings to the config file and removed unnesesary 'this.' and configured PID controllers from a config. Added 3 methods.

---
+++ src/main/java/com/team766/robot/reva_2025/constants/ConfigConstants.java
@@ -0,0 +1,10 @@
+package com.team766.robot.reva_2025.constants;
+
+public final class ConfigConstants {
+    // utility class
+    private ConfigConstants() {}
+
+    public static final String DRIVE_TARGET_TRANSLATION_PID = "drive.setpointPidTranslation";
+    public static final String DRIVE_TARGET_ROTATION_PID = "drive.setpointPidRotation";
+    
+}
--- src/main/java/com/team766/robot/reva_2025/procedures/AutoAlign.java
+++ src/main/java/com/team766/robot/reva_2025/procedures/AutoAlign.java
@@ -1,10 +1,10 @@
 package com.team766.robot.reva_2025.procedures;
 
 import com.team766.controllers.PIDController;
-import com.team766.framework.Context;
-import com.team766.framework.Procedure;
+import com.team766.framework3.Context;
+import com.team766.framework3.Procedure;
 import com.team766.robot.common.mechanisms.SwerveDrive;
-import com.team766.robot.reva_2025.Robot;
+import com.team766.robot.reva_2025.constants.ConfigConstants;
 import edu.wpi.first.math.geometry.Pose2d;
 
 public class AutoAlign extends Procedure {
@@ -13,50 +13,44 @@ public class AutoAlign extends Procedure {
     private PIDController pidControllerX;
     private PIDController pidControllerY;
     private PIDController pidControllerRotation;
-    private double P_translation;
-    private double I_translation;
-    private double D_translation;
-    private double P_rotation;
-    private double I_rotation;
-    private double D_rotation;
-    private double threshold_translation;
-    private double threshold_rotation; 
 
     public AutoAlign(Pose2d targetPosition, SwerveDrive drive) {
         this.targetPosition = targetPosition;
-        this.drive = drive;
-        this.P_translation = 0;
-        this.I_translation = 0;
-        this.D_translation = 0;
-        this.P_rotation = 0;
-        this.I_rotation = 0;
-        this.D_rotation = 0;
-        this.threshold = 0.01;
-        this.threshold_translation = 0.01;
-        this.threshold_rotation = 0.01;
-        this.pidControllerX = new PIDController(this.P_translation, this.I_translation, this.D_translation, 0, 1, this.threshold_translation);
-        this.pidControllerY = new PIDController(this.P_translation, this.I_translation, this.D_translation, 0, 1, this.threshold_translation);
-        this.pidControllerRotation = new PIDController(this.P_rotation, this.I_rotation, this.D_rotation, -1, 1, this.threshold_rotation);  
+        this.drive = reserve(drive);
+        pidControllerY = PIDController.loadFromConfig(ConfigConstants.DRIVE_TARGET_TRANSLATION_PID);
+        pidControllerY = PIDController.loadFromConfig(ConfigConstants.DRIVE_TARGET_TRANSLATION_PID);
+        pidControllerRotation = PIDController.loadFromConfig(ConfigConstants.DRIVE_TARGET_ROTATION_PID);
     }
+
+    public double getXPIDOutput() {
+        return pidControllerX.getOutput();
+    }
+    
+    public double getYPIDOutput() {
+        return pidControllerY.getOutput();
+    }
+
+    public double getRotationPIDOutput() {
+        return pidControllerRotation.getOutput();
+    }
+
     public void run(Context context) {
         Pose2d currentPosition;
         double currentHeading;  
 
-        context.takeOwnership(this.drive);
-        this.pidControllerX.setSetpoint(targetPosition.getX());
-        this.pidControllerY.setSetpoint(targetPosition.getY());
-        this.pidControllerRotation.setSetpoint(targetPosition.getRotation().getDegrees());
-        while (!this.pidControllerX.isDone() || !this.pidControllerY.isDone() || !this.pidControllerRotation.isDone()) {
-            currentPosition = this.drive.getCurrentPosition();
-            currentHeading = this.drive.getHeading();
-            this.pidControllerX.calculate(currentPosition.getX());
-            this.pidControllerY.calculate(currentPosition.getY());
-            this.pidControllerRotation.calculate(currentHeading);
-            this.drive.controlFieldOriented(this.pidControllerX.getOutput(), this.pidControllerY.getOutput(), this.pidControllerRotation.getOutput());
+        pidControllerX.setSetpoint(targetPosition.getX());
+        pidControllerY.setSetpoint(targetPosition.getY());
+        pidControllerRotation.setSetpoint(targetPosition.getRotation().getDegrees());
+        while (!pidControllerX.isDone() || !pidControllerY.isDone() || !pidControllerRotation.isDone()) {
+            currentPosition = getStatusOrThrow(SwerveDrive.DriveStatus.class).currentPosition();
+            currentHeading =  getStatusOrThrow(SwerveDrive.DriveStatus.class).heading();
+            pidControllerX.calculate(currentPosition.getX());
+            pidControllerY.calculate(currentPosition.getY());
+            pidControllerRotation.calculate(currentHeading);
+            drive.controlFieldOriented(pidControllerX.getOutput(), pidControllerY.getOutput(), pidControllerRotation.getOutput());
             context.yield();
         }
-        this.drive.stopDrive();
-        this.drive.setCross();
+        drive.stopDrive();
     } 
 
 }
