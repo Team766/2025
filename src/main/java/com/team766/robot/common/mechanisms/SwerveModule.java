a529ec496f9e50b668082c5d486c3f9c7ec3f680
xavier <avier@gmail.com>
2025-01-25 14:18:41 -0800

    merged code

--- src/main/java/com/team766/robot/common/mechanisms/SwerveModule.java
+++ src/main/java/com/team766/robot/common/mechanisms/SwerveModule.java
@@ -3,9 +3,6 @@ package com.team766.robot.common.mechanisms;
 import com.team766.hal.EncoderReader;
 import com.team766.hal.MotorController;
 import com.team766.hal.MotorController.ControlMode;
-import com.team766.logging.Category;
-import com.team766.logging.Logger;
-import com.team766.logging.Severity;
 import com.team766.robot.common.SwerveConfig;
 import com.team766.robot.reva.mechanisms.MotorUtil;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -86,18 +83,8 @@ public class SwerveModule {
     }
 
     private double computeEncoderOffset() {
-        StatusSignal<Angle> value = encoder.getAbsolutePosition();
-        if (!value.getStatus().isOK()) {
-            Logger.get(Category.DRIVE)
-                    .logData(
-                            Severity.ERROR,
-                            "%s unable to read encoder: %s",
-                            modulePlacement,
-                            value.getStatus().toString());
-            return 0; // ??
-        }
         return (steer.getSensorPosition() / encoderConversionFactor) % 360
-                - (value.getValueAsDouble() * 360);
+                - (encoder.getPosition() * 360);
     }
 
     /**
@@ -106,7 +93,7 @@ public class SwerveModule {
      * @param vector the vector specifying the module's motion
      */
     public void steer(Vector2D vector) {
-        boolean reversed = false;
+        //boolean reversed = false;
         SmartDashboard.putString(
                 "[" + modulePlacement + "]" + "x, y",
                 String.format("%.2f, %.2f", vector.getX(), vector.getY()));
