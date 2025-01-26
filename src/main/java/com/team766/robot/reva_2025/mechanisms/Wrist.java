33c72da2585aeaff876b31be63b95453541de666
Driver Station Computer <rzghosh@gmail.com>
2025-01-25 16:16:51 -0800

    Updated Wrist code for Maroon Framework 3

--- src/main/java/com/team766/robot/reva_2025/mechanisms/Wrist.java
+++ src/main/java/com/team766/robot/reva_2025/mechanisms/Wrist.java
@@ -1,40 +1,25 @@
 package com.team766.robot.reva_2025.mechanisms;
 
-import com.team766.framework.Mechanism;
+import com.team766.framework3.Mechanism;
 import com.team766.hal.MotorController;
 import com.team766.hal.RobotProvider;
 
-public class Wrist extends Mechanism {
+public class Wrist 
+    extends MechanismWithStatus<Wrist.WristStatus> {
 
-    public enum WristPosition {
+    public record WristStatus(double angle) {}
 
-        // TODO: update all of these to real things!
-        PICKUP_CORAL(0),
-        PLACE_CORAL(180);
-
-        private final double angle;
-
-        private WristPosition(double angle) {
-            this.angle = angle;
-        }
-
-        public double getAngle() {
-            return angle;
-        }
-    }
+    private double angle;
 
     private MotorController wristMotor;
 
     public Wrist() {
         wristMotor = RobotProvider.instance.getMotor("Wrist.Motor");
-    }
-
-    public void setAngle(WristPosition position) {
-        setAngle(position.getAngle());
+        angle = 0;
     }
 
     public void setAngle(double angle) {
-        checkContextOwnership();
         wristMotor.set(MotorController.ControlMode.Position, angle);
+        this.angle = angle;
     }
 }
