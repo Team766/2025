442a11ea34287f57b3e103de0c837e5685fbcb31
Driver Station Computer <rzghosh@gmail.com>
2025-01-25 16:22:47 -0800

    ran spotless apply and fixed missing imports

--- src/main/java/com/team766/robot/reva_2025/mechanisms/Wrist.java
+++ src/main/java/com/team766/robot/reva_2025/mechanisms/Wrist.java
@@ -1,13 +1,13 @@
 package com.team766.robot.reva_2025.mechanisms;
 
-import com.team766.framework3.Mechanism;
+import com.team766.framework3.MechanismWithStatus;
+import com.team766.framework3.Status;
 import com.team766.hal.MotorController;
 import com.team766.hal.RobotProvider;
 
-public class Wrist 
-    extends MechanismWithStatus<Wrist.WristStatus> {
+public class Wrist extends MechanismWithStatus<Wrist.WristStatus> {
 
-    public record WristStatus(double angle) {}
+    public record WristStatus(double angle) implements Status {}
 
     private double angle;
 
