067949b813dcc4aa54bf4d17a44b3656b17335c2
xavier <avier@gmail.com>
2025-01-27 19:25:29 -0800

    Added enum to elevator and commetned out in EncoderUtils

--- src/main/java/com/team766/robot/reva_2025/mechanisms/CoralIntake.java
+++ src/main/java/com/team766/robot/reva_2025/mechanisms/CoralIntake.java
@@ -6,35 +6,30 @@ import com.team766.hal.MotorController;
 import com.team766.hal.RobotProvider;
 
 public class CoralIntake extends MechanismWithStatus<CoralIntake.CoralIntakeStatus> {
-
     private static final double POWER_IN = 1.0;
     private static final double POWER_OUT = -1.0;
-    private double currentPower;
     private MotorController motor;
 
     public static record CoralIntakeStatus(double currentPower) implements Status {}
 
     public CoralIntake() {
         motor = RobotProvider.instance.getMotor("coralIntake.motor");
-        currentPower = 0;
     }
 
     public void in() {
         motor.set(POWER_IN);
-        currentPower = POWER_IN;
     }
 
     public void out() {
         motor.set(POWER_OUT);
-        currentPower = POWER_OUT;
     }
 
     public void stop() {
         motor.set(0.0);
-        currentPower = 0;
     }
 
+    @Override
     protected CoralIntakeStatus updateStatus() {
-        return new CoralIntakeStatus(currentPower);
+        return new CoralIntakeStatus(motor.get());
     }
 }
--- src/main/java/com/team766/robot/reva_2025/mechanisms/Elevator.java
+++ src/main/java/com/team766/robot/reva_2025/mechanisms/Elevator.java
@@ -8,28 +8,44 @@ import com.team766.hal.RobotProvider;
 public class Elevator extends MechanismWithStatus<Elevator.ElevatorStatus> {
     private MotorController elevatorLeftMotor;
     private MotorController elevatorRightMotor;
-    private double currentPosition;
-    private final double MIN_HEIGHT = 0;
-    private final double MAX_HEIGHT = 150;
-    private final double NUDGE_AMOUNT = 5;
+    private static double MIN_HEIGHT = 0;
+    private static double MAX_HEIGHT = 150;
+    private static double NUDGE_AMOUNT = 5;
 
     // values are untested and are set to
 
-    public static record ElevatorStatus(double currentPosition) implements Status {}
+    public static record ElevatorStatus(double currentHeight) implements Status {}
+
+    public enum Position {
+        ELEVATOR_TOP(MAX_HEIGHT),
+        ELEVATOR_BOTTOM(MIN_HEIGHT);
+
+        private double height;
+
+        Position(double height) {
+            this.height = height;
+        }
+
+        public double getHeight() {
+            return height;
+        }
+    }
 
     public Elevator() {
         elevatorLeftMotor = RobotProvider.instance.getMotor("elevator.leftMotor");
         elevatorRightMotor = RobotProvider.instance.getMotor("elevator.rightMotor");
         elevatorRightMotor.follow(elevatorLeftMotor);
-        currentPosition = 0;
         elevatorLeftMotor.setSensorPosition(0);
     }
 
     public void setPosition(double setPosition) {
         if (setPosition >= MIN_HEIGHT && setPosition <= MAX_HEIGHT) {
             elevatorLeftMotor.set(MotorController.ControlMode.Position, setPosition);
-            currentPosition = setPosition;
         }
+    }
+
+    public void setPosition(Position position) {
+        setPosition(position.getHeight());
     }
 
     public void nudgeUp() {
@@ -42,7 +58,9 @@ public class Elevator extends MechanismWithStatus<Elevator.ElevatorStatus> {
         setPosition(nudgePosition);
     }
 
+    @Override
     protected ElevatorStatus updateStatus() {
-        return new ElevatorStatus(currentPosition);
+        return new ElevatorStatus(
+                EncoderUtils.elevatorRotationsToHeight(elevatorLeftMotor.getSensorPosition()));
     }
 }
