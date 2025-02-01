03340d70a96e7971a0a87dc021677835f3230fcf
Computer <team766robotics@gmail.com>
2025-01-31 20:28:13 -0800

    test auto

---
+++ src/main/java/com/team766/robot/reva_2025/procedures/IntakeCoral.java
@@ -0,0 +1,8 @@
+package com.team766.robot.reva_2025.procedures;
+
+import com.team766.framework3.Context;
+import com.team766.framework3.Procedure;
+
+public class IntakeCoral extends Procedure {
+    public void run(Context context) {}
+}
---
+++ src/main/java/com/team766/robot/reva_2025/procedures/autons/TestAuto.java
@@ -0,0 +1,25 @@
+package com.team766.robot.reva_2025.procedures.autons;
+
+import com.team766.robot.common.mechanisms.SwerveDrive;
+import com.team766.robot.common.procedures.PathSequenceAuto;
+import com.team766.robot.reva_2025.constants.CoralConstants.CoralConstant;
+import com.team766.robot.reva_2025.mechanisms.CoralIntake;
+import com.team766.robot.reva_2025.mechanisms.Elevator;
+import com.team766.robot.reva_2025.mechanisms.Wrist;
+import com.team766.robot.reva_2025.procedures.IntakeCoral;
+import com.team766.robot.reva_2025.procedures.ScoreCoral;
+import edu.wpi.first.math.geometry.Pose2d;
+
+public class TestAuto extends PathSequenceAuto {
+    public TestAuto(SwerveDrive drive, Elevator elevator, Wrist wrist, CoralIntake intake) {
+        super(drive, new Pose2d());
+        addPath("Inner Cage Start Blue A to Reef 2");
+        addProcedure(new ScoreCoral(CoralConstant.A_B, 0, 0, drive, elevator, wrist, intake));
+        addPath("Reef 2 to Coral Station 1");
+        addProcedure(new IntakeCoral());
+        addPath("Coral Station 1 to Reef 3");
+        addProcedure(new ScoreCoral(CoralConstant.C_B, 0, 0, drive, elevator, wrist, intake));
+        addPath("Reef 3 to Coral Station 1");
+        //^^about 15 secs^^
+    }
+}
