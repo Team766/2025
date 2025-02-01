e3d894f45f30e1c7c00d3a46d488e8c74c6efc28
Computer <team766robotics@gmail.com>
2025-02-01 15:34:17 -0800

    Updating Input Constants (not ready for review yet)

--- src/main/java/com/team766/robot/reva_2025/constants/InputConstants.java
+++ src/main/java/com/team766/robot/reva_2025/constants/InputConstants.java
@@ -11,4 +11,17 @@ public final class InputConstants {
     public static final int BUTTON_LEVEL23_LEVEL = 7;
     public static final int BUTTON_LEVEL34_LEVEL = 8;
     public static final int BUTTON_ON_SHOOTER = 7;
+    public static final int GAMEPAD_A_BUTTON = 1;
+    public static final int GAMEPAD_B_BUTTON = 2;
+    public static final int GAMEPAD_X_BUTTON = 3;
+    public static final int GAMEPAD_Y_BUTTON = 4;
+    public static final int RIGHT_TRIGGER = 3;
+    public static final int LEFT_TRIGGER = 2;
+    public static final int RIGHT_BUTTON = 6;
+    public static final int LEFT_BUTTON = 5;
+    public static final int DPAD_DOWN = 180;
+    public static final int DPAD_RIGHT = 90;
+    public static final int DPAD_LEFT = 270;
+    public static final int DPAD_UP = 0;
+    public static final int BackButton = 7;
 }
