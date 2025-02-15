b56d3d4b4c1e82d7f0237dd2b0b7a3b1d63ae5a1
pm818 <preeyamerchant818@gmail.com>
2025-02-14 19:38:46 -0800

    Fixed build by using MotorUtil for soft limits

--- src/main/java/com/team766/robot/reva_2025/mechanisms/Climber.java
+++ src/main/java/com/team766/robot/reva_2025/mechanisms/Climber.java
@@ -1,9 +1,14 @@
 package com.team766.robot.reva_2025.mechanisms;
 
+import static com.team766.robot.reva.constants.ConfigConstants.CLIMBER_LEFT_MOTOR;
+import static com.team766.robot.reva.constants.ConfigConstants.CLIMBER_RIGHT_MOTOR;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.team766.framework3.MechanismWithStatus;
 import com.team766.framework3.Status;
 import com.team766.hal.MotorController;
 import com.team766.hal.RobotProvider;
+import com.team766.robot.reva.mechanisms.MotorUtil;
 
 public class Climber extends MechanismWithStatus<Climber.ClimberStatus> {
     private MotorController leftClimberMotor;
@@ -19,12 +24,12 @@ public class Climber extends MechanismWithStatus<Climber.ClimberStatus> {
         rightClimberMotor = RobotProvider.instance.getMotor(CLIMBER_RIGHT_MOTOR);
         leftClimberMotor.setNeutralMode(NeutralMode.Brake);
         rightClimberMotor.setNeutralMode(NeutralMode.Brake);
-        leftClimberMotor.setSoftLimits(HIGH_LIMIT, 0);
-        rightClimberMotor.setSoftLimits(HIGH_LIMIT, 0);
-        leftClimberMotor.enableSoftLimits(true);
-        rightClimberMotor.enableSoftLimits(true);
+        MotorUtil.setSoftLimits(leftClimberMotor, HIGH_LIMIT, 0);
+        MotorUtil.setSoftLimits(rightClimberMotor, HIGH_LIMIT, 0);
+        MotorUtil.enableSoftLimits(leftClimberMotor, true);
+        MotorUtil.enableSoftLimits(rightClimberMotor, true);
         rightClimberMotor.follow(leftClimberMotor);
-        leftClimberMotor.setEncoderPosition(0);
+        leftClimberMotor.setSensorPosition(0);
     }
 
     public void climbUp() {
