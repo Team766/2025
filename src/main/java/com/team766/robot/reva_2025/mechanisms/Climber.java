58954c841e25d35c99436042c9d6322d87615cd7
pm818 <preeyamerchant818@gmail.com>
2025-02-14 18:36:42 -0800

    Updated the changes for the climber: added constants for up and down, and set the encoder value to 0, also moved the follow to after in the Climber constructor so it doesn't have a problem.

--- src/main/java/com/team766/robot/reva_2025/mechanisms/Climber.java
+++ src/main/java/com/team766/robot/reva_2025/mechanisms/Climber.java
@@ -9,27 +9,30 @@ public class Climber extends MechanismWithStatus<Climber.ClimberStatus> {
     private MotorController leftClimberMotor;
     private MotorController rightClimberMotor;
     private double HIGH_LIMIT = 90;
+    private double UP_POWER = 0.5;
+    private double DOWN_POWER = -0.5;
 
     public static record ClimberStatus(double currentPower) implements Status {}
 
     public Climber() {
         leftClimberMotor = RobotProvider.instance.getMotor(CLIMBER_LEFT_MOTOR);
         rightClimberMotor = RobotProvider.instance.getMotor(CLIMBER_RIGHT_MOTOR);
-        rightClimberMotor.follow(leftClimberMotor);
         leftClimberMotor.setNeutralMode(NeutralMode.Brake);
         rightClimberMotor.setNeutralMode(NeutralMode.Brake);
         leftClimberMotor.setSoftLimits(HIGH_LIMIT, 0);
         rightClimberMotor.setSoftLimits(HIGH_LIMIT, 0);
         leftClimberMotor.enableSoftLimits(true);
         rightClimberMotor.enableSoftLimits(true);
+        rightClimberMotor.follow(leftClimberMotor);
+        leftClimberMotor.setEncoderPosition(0);
     }
 
     public void climbUp() {
-        leftClimberMotor.set(1);
+        leftClimberMotor.set(UP_POWER);
     }
 
     public void climbDown() {
-        leftClimberMotor.set(-1);
+        leftClimberMotor.set(DOWN_POWER);
     }
 
     public void climbOff() {
