045c69059fe18cdf68ca3e65c3905d4bb32e39db
pm818 <121070520+pm818@users.noreply.github.com>
2025-02-15 18:00:05 -0800

    Mechanism update (#52)
    ## Description
    
    Added run methods into Elevator and Wrist. moved isAtAngle into
    WristStatus and moved isAtHeight into ElevatorStatus. Deleted redundant
    code from AlgaeIntake. Created necessary config constants.
    
    ## How Has This Been Tested? Not tested yet 
    
    - [ ] Unit tests: [Add your description here]
    - [ ] Simulator testing: [Add your description here]
    - [ ] On-robot bench testing: [Add your description here]
    - [ ] On-robot field testing: [Add your description here]

--- src/main/java/com/team766/robot/reva_2025/mechanisms/Elevator.java
+++ src/main/java/com/team766/robot/reva_2025/mechanisms/Elevator.java
@@ -4,23 +4,28 @@ import com.team766.framework3.MechanismWithStatus;
 import com.team766.framework3.Status;
 import com.team766.hal.MotorController;
 import com.team766.hal.RobotProvider;
+import com.team766.robot.reva_2025.constants.ConfigConstants;
 
 public class Elevator extends MechanismWithStatus<Elevator.ElevatorStatus> {
     private MotorController elevatorLeftMotor;
     private MotorController elevatorRightMotor;
-    private static final double MIN_HEIGHT = 0;
-    private static final double MAX_HEIGHT = 150;
     private static final double NUDGE_AMOUNT = 5;
+    private static final double POSITION_LOCATION_THRESHOLD = 1;
     private double setPoint;
     private final double thresholdConstant = 0; // TODO: Update me after testing!
 
     // values are untested and are set to
 
-    public static record ElevatorStatus(double currentHeight) implements Status {}
+    public static record ElevatorStatus(double currentHeight, double targetHeight)
+            implements Status {
+        public boolean isAtHeight() {
+            return Math.abs(targetHeight() - currentHeight()) < POSITION_LOCATION_THRESHOLD;
+        }
+    }
 
     public enum Position {
-        ELEVATOR_TOP(EncoderUtils.elevatorRotationsToHeight(MAX_HEIGHT)),
-        ELEVATOR_BOTTOM(EncoderUtils.elevatorRotationsToHeight(MIN_HEIGHT));
+        ELEVATOR_TOP(22),
+        ELEVATOR_BOTTOM(0);
 
         private double height;
 
@@ -38,18 +43,19 @@ public class Elevator extends MechanismWithStatus<Elevator.ElevatorStatus> {
     }
 
     public Elevator() {
-        elevatorLeftMotor = RobotProvider.instance.getMotor("elevator.leftMotor");
-        elevatorRightMotor = RobotProvider.instance.getMotor("elevator.rightMotor");
+        elevatorLeftMotor = RobotProvider.instance.getMotor(ConfigConstants.LEFT_ELEVATOR_MOTOR);
+        elevatorRightMotor = RobotProvider.instance.getMotor(ConfigConstants.RIGHT_ELEVATOR_MOTOR);
         elevatorRightMotor.follow(elevatorLeftMotor);
         elevatorLeftMotor.setSensorPosition(0);
         setPoint = 0;
     }
 
     public void setPosition(double setPosition) {
-        if (setPosition >= MIN_HEIGHT && setPosition <= MAX_HEIGHT) {
-            setPoint = setPosition;
-            elevatorLeftMotor.set(MotorController.ControlMode.Position, setPosition);
-        }
+        setPoint =
+                com.team766.math.Math.clamp(
+                        setPosition,
+                        Position.ELEVATOR_BOTTOM.getHeight(),
+                        Position.ELEVATOR_TOP.getHeight());
     }
 
     public void setPosition(Position position) {
@@ -67,12 +73,16 @@ public class Elevator extends MechanismWithStatus<Elevator.ElevatorStatus> {
     }
 
     @Override
-    protected ElevatorStatus updateStatus() {
-        return new ElevatorStatus(
-                EncoderUtils.elevatorRotationsToHeight(elevatorLeftMotor.getSensorPosition()));
+    protected void run() {
+        elevatorLeftMotor.set(
+                MotorController.ControlMode.Position,
+                EncoderUtils.elevatorHeightToRotations(setPoint));
     }
 
-    public boolean isAtPosition() {
-        return (Math.abs(setPoint - elevatorLeftMotor.getSensorPosition()) < thresholdConstant);
+    @Override
+    protected ElevatorStatus updateStatus() {
+        return new ElevatorStatus(
+                EncoderUtils.elevatorRotationsToHeight(elevatorLeftMotor.getSensorPosition()),
+                setPoint);
     }
 }
--- src/main/java/com/team766/robot/reva_2025/mechanisms/Wrist.java
+++ src/main/java/com/team766/robot/reva_2025/mechanisms/Wrist.java
@@ -1,16 +1,25 @@
 package com.team766.robot.reva_2025.mechanisms;
 
+import com.team766.config.ConfigFileReader;
 import com.team766.framework3.MechanismWithStatus;
 import com.team766.framework3.Status;
 import com.team766.hal.MotorController;
 import com.team766.hal.RobotProvider;
+import com.team766.library.ValueProvider;
+import com.team766.robot.reva_2025.constants.ConfigConstants;
 
 public class Wrist extends MechanismWithStatus<Wrist.WristStatus> {
 
     private final double thresholdConstant = 0; // TODO: Update me after testing!
     private double setPoint;
+    private static final double POSITION_LOCATION_THRESHOLD = 1;
+    private final ValueProvider<Double> ffGain;
 
-    public record WristStatus(double angle) implements Status {}
+    public record WristStatus(double currentAngle, double targetAngle) implements Status {
+        public boolean isAtAngle() {
+            return Math.abs(targetAngle() - currentAngle()) < POSITION_LOCATION_THRESHOLD;
+        }
+    }
 
     public enum WristPosition {
 
@@ -32,7 +41,8 @@ public class Wrist extends MechanismWithStatus<Wrist.WristStatus> {
     private MotorController wristMotor;
 
     public Wrist() {
-        wristMotor = RobotProvider.instance.getMotor("Wrist.Motor");
+        wristMotor = RobotProvider.instance.getMotor(ConfigConstants.WRIST_MOTOR);
+        ffGain = ConfigFileReader.getInstance().getDouble(ConfigConstants.WRIST_FFGAIN);
     }
 
     public void setAngle(WristPosition position) {
@@ -41,15 +51,20 @@ public class Wrist extends MechanismWithStatus<Wrist.WristStatus> {
 
     public void setAngle(double angle) {
         setPoint = angle;
-        wristMotor.set(MotorController.ControlMode.Position, angle);
+    }
+
+    @Override
+    protected void run() {
+        double ff = ffGain.valueOr(0.0) * Math.cos(Math.toRadians(getStatus().currentAngle()));
+        wristMotor.set(
+                MotorController.ControlMode.Position,
+                EncoderUtils.coralWristDegreesToRotations(setPoint),
+                ff);
     }
 
     @Override
     protected WristStatus updateStatus() {
-        return new WristStatus(wristMotor.get());
-    }
-
-    public boolean isAtPosition() {
-        return (Math.abs(setPoint - wristMotor.getSensorPosition()) < thresholdConstant);
+        return new WristStatus(
+                EncoderUtils.coralWristRotationsToDegrees(wristMotor.get()), setPoint);
     }
 }
