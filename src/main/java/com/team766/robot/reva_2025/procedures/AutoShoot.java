0cbcbfe6a1dbc37e03602650aad6e7392474817b
lydski <lydslowski@gmail.com>
2025-03-01 16:22:42 -0800

    added findAngle based on position, added AnywhereScoringPosition, added Transform3d method.

--- src/main/java/com/team766/robot/reva_2025/procedures/AutoShoot.java
+++ src/main/java/com/team766/robot/reva_2025/procedures/AutoShoot.java
@@ -1,17 +1,34 @@
 package com.team766.robot.reva_2025.procedures;
 
+import com.team766.ViSIONbase.AnywhereScoringPosition;
 import com.team766.framework3.Context;
 import com.team766.framework3.Procedure;
+import com.team766.logging.Severity;
 import com.team766.robot.common.mechanisms.SwerveDrive;
+import com.team766.robot.reva.mechanisms.ForwardApriltagCamera;
 import com.team766.robot.reva_2025.mechanisms.AlgaeIntake;
 import com.team766.robot.reva_2025.mechanisms.AlgaeIntake.Level;
 import com.team766.robot.reva_2025.mechanisms.AlgaeIntake.State;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Transform3d;
+import java.util.ArrayList;
+import java.util.Optional;
 
 public class AutoShoot extends Procedure {
     private AlgaeIntake algaeIntake;
     private SwerveDrive drive;
     private Pose2d pose2D;
+    private final boolean useSetPosition = false;
+    protected static ArrayList<AnywhereScoringPosition> shootingPositions =
+            new ArrayList<AnywhereScoringPosition>() {
+                {
+                    add(new AnywhereScoringPosition(1.9506, 4800, 23.516));
+                    add(new AnywhereScoringPosition(2.072, 4800, 27.32));
+                    add(new AnywhereScoringPosition(2.29161, 4800, 30.109));
+                    add(new AnywhereScoringPosition(2.4616, 4800, 30.8987));
+                    add(new AnywhereScoringPosition(2.6942, 4800, 32.699));
+                }
+            };
 
     public AutoShoot(Pose2d pose2D, AlgaeIntake algaeIntake, SwerveDrive drive) {
         this.algaeIntake = reserve(algaeIntake);
@@ -21,14 +38,55 @@ public class AutoShoot extends Procedure {
 
     @Override
     public void run(Context context) {
+        if (useSetPosition) {
+            shootFromSetPosition(context);
+        } else {
+            shootFromCurrentPosition(context);
+        }
+    }
+
+    public void shootFromSetPosition(Context context) {
         algaeIntake.setArmAngle(Level.Shoot);
         context.runSync(new AutoAlign(pose2D, drive));
-        // use auto allign to move to a position
-        // move shoot arm to correct position
         waitForStatusMatching(context, AlgaeIntake.AlgaeIntakeStatus.class, s -> s.isAtAngle());
-        // shoot
         algaeIntake.setState(State.Shoot);
         context.waitForSeconds(1);
         algaeIntake.setState(State.Idle);
+    }
+
+    public void shootFromCurrentPosition(Context context) {
+        drive.stopDrive();
+        Optional<Transform3d> maybeToUse =
+                context.waitForValueOrTimeout(this::getTransform3dOfRobotToTag, 1.0);
+        if (!maybeToUse.isPresent()) {
+            log(Severity.ERROR, "No tag info available");
+            return;
+        }
+        Transform3d toUse = maybeToUse.orElseThrow();
+        double distanceOfRobotToTag =
+                Math.sqrt(Math.pow(toUse.getX(), 2) + Math.pow(toUse.getY(), 2));
+        double bestAngle = getBestAngleToUse(distanceOfRobotToTag);
+        if (bestAngle > -1) {
+            algaeIntake.setArmAngle(bestAngle);
+            waitForStatusMatching(context, AlgaeIntake.AlgaeIntakeStatus.class, s -> s.isAtAngle());
+            algaeIntake.setState(State.Shoot);
+            context.waitForSeconds(1);
+            algaeIntake.setState(State.Idle);
+        }
+    }
+
+    private Optional<Transform3d> getTransform3dOfRobotToTag() {
+        return getStatusOrThrow(ForwardApriltagCamera.ApriltagCameraStatus.class)
+                .speakerTagTransform();
+    }
+
+    public static double getBestAngleToUse(double distanceFromCenterApriltag) {
+        for (int i = 0; i < shootingPositions.size(); i++) {
+            if (distanceFromCenterApriltag
+                    <= shootingPositions.get(i).distanceFromCenterApriltag()) {
+                return shootingPositions.get(i).angleToSetArm();
+            }
+        }
+        return -1;
     }
 }
