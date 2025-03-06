bf27f0c335d4d45d0b4b102dda3fd8e84b00fc55
dejabot <104333734+dejabot@users.noreply.github.com>
2025-03-05 21:17:06 -0800

    add ambient signal to TOF (#59)
    ## Description
    
    Add ambient signal to TOF interface. This was useful in using the
    CANRange TOF for algae intake.
    
    PWF supports something similar, tho the units may be different.
    
    ## How Has This Been Tested?
    
    This specific code path needs to be tested; we tested usage of the
    underlying CANRange impl at the practice field.
    
    - [ ] Unit tests: [Add your description here]
    - [ ] Simulator testing: [Add your description here]
    - [ ] On-robot bench testing: [Add your description here]
    - [ ] On-robot field testing: [Add your description here]

--- src/main/java/com/team766/hal/mock/MockTimeOfFlight.java
+++ src/main/java/com/team766/hal/mock/MockTimeOfFlight.java
@@ -1,15 +1,17 @@
 package com.team766.hal.mock;
 
 import com.team766.hal.TimeOfFlightReader;
+import java.util.Optional;
 
 public class MockTimeOfFlight implements TimeOfFlightReader {
-    private double distance = 0.0;
+    private Optional<Double> distance = Optional.empty();
     private boolean valid = false;
+    private Optional<Double> ambientSignal = Optional.empty();
 
     public MockTimeOfFlight() {}
 
     @Override
-    public double getDistance() {
+    public Optional<Double> getDistance() {
         return distance;
     }
 
@@ -19,13 +21,22 @@ public class MockTimeOfFlight implements TimeOfFlightReader {
     }
 
     @Override
+    public Optional<Double> getAmbientSignal() {
+        return ambientSignal;
+    }
+
+    @Override
     public void setRange(TimeOfFlightReader.Range range) {}
 
     public void setDistance(final double distance_) {
-        this.distance = distance_;
+        this.distance = Optional.of(distance_);
     }
 
     public void setLastMeasurementValid(final boolean valid_) {
         this.valid = valid_;
+    }
+
+    public void setAmbientSignal(final double ambientSignal) {
+        this.ambientSignal = Optional.of(ambientSignal);
     }
 }
