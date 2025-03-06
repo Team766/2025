fe81af1486f6bb5c272e504ae9c669459016cf0a
lydski <lydslowski@gmail.com>
2025-03-05 18:55:41 -0800

    Ran SpotlessApply

--- src/main/java/com/team766/robot/reva_2025/procedures/AutoShoot.java
+++ src/main/java/com/team766/robot/reva_2025/procedures/AutoShoot.java
@@ -18,7 +18,8 @@ import java.util.Optional;
 public class AutoShoot extends Procedure {
     private AlgaeIntake algaeIntake;
     private SwerveDrive drive;
-    private Pose2d setPosition2D = new Pose2d(5.0,5.0, new Rotation2d(90.0)); // TODO: update with actual values
+    private Pose2d setPosition2D =
+            new Pose2d(5.0, 5.0, new Rotation2d(90.0)); // TODO: update with actual values
     private final boolean useSetPosition = true;
     protected static ArrayList<AnywhereScoringPosition> shootingPositions =
             new ArrayList<AnywhereScoringPosition>() {
