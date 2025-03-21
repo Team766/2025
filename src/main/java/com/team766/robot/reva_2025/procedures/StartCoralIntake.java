c4ba7bc17d88608a1506b6abb2a2f8dd7eb50eb6
qntmcube <100043837+qntmcube@users.noreply.github.com>
2025-03-20 23:54:14 -0700

    paths

--- src/main/java/com/team766/robot/reva_2025/procedures/StartCoralIntake.java
+++ src/main/java/com/team766/robot/reva_2025/procedures/StartCoralIntake.java
@@ -3,21 +3,15 @@ package com.team766.robot.reva_2025.procedures;
 import com.team766.framework3.Context;
 import com.team766.framework3.Procedure;
 import com.team766.robot.reva_2025.mechanisms.CoralIntake;
-import com.team766.robot.reva_2025.mechanisms.Elevator;
-import com.team766.robot.reva_2025.mechanisms.Wrist;
 
 public class StartCoralIntake extends Procedure {
-    private Elevator elevator;
-    private Wrist wrist;
     private CoralIntake coralIntake;
 
-    public StartCoralIntake(Elevator elevator, Wrist wrist, CoralIntake coralIntake) {
-        this.elevator = reserve(elevator);
-        this.wrist = reserve(wrist);
+    public StartCoralIntake(CoralIntake coralIntake) {
         this.coralIntake = reserve(coralIntake);
     }
 
     public void run(Context context) {
-        context.runParallel(new CoralStationPositionAndIntake(elevator, wrist, coralIntake));
+        coralIntake.in();
     }
 }
