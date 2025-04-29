Team766/2024

MF3 demo bots (v1)

src/main/java/com/team766/robot/burro_arm/Robot.java
@@ -1,16 +1,16 @@
 package com.team766.robot.burro_arm;
 
 import com.team766.framework.AutonomousMode;
-import com.team766.framework.Procedure;
+import com.team766.framework.RuleEngine;
 import com.team766.hal.RobotConfigurator;
 import com.team766.robot.burro_arm.mechanisms.*;
 import com.team766.robot.common.mechanisms.BurroDrive;
 
 public class Robot implements RobotConfigurator {
     // Declare mechanisms (as static fields) here
-    public static BurroDrive drive;
-    public static Arm arm;
-    public static Gripper gripper;
+    private BurroDrive drive;
+    private Arm arm;
+    private Gripper gripper;
 
     @Override
     public void initializeMechanisms() {
@@ -21,8 +21,13 @@ public class Robot implements RobotConfigurator {
     }
 
     @Override
-    public Procedure createOI() {
-        return new OI();
+    public RuleEngine createOI() {
+        return new OI(drive, arm, gripper);
+    }
+
+    @Override
+    public RuleEngine createLights() {
+        return null;
     }
 
     @Override

src/main/java/com/team766/robot/burro_arm/mechanisms/Gripper.java
@@ -2,37 +2,79 @@ package com.team766.robot.burro_arm.mechanisms;
 
 import com.team766.config.ConfigFileReader;
 import com.team766.framework.Mechanism;
+import com.team766.framework.Request;
+import com.team766.framework.Status;
 import com.team766.hal.MotorController;
 import com.team766.hal.RobotProvider;
 
-public class Gripper extends Mechanism {
+public class Gripper extends Mechanism<Gripper.GripperRequest, Gripper.GripperStatus> {
+    public record GripperStatus() implements Status {}
+
+    public sealed interface GripperRequest extends Request {}
+
+    public record Idle() implements GripperRequest {
+        @Override
+        public boolean isDone() {
+            return true;
+        }
+    }
+
+    public record Intake() implements GripperRequest {
+        @Override
+        public boolean isDone() {
+            return true;
+        }
+    }
+
+    public record Outtake() implements GripperRequest {
+        @Override
+        public boolean isDone() {
+            return true;
+        }
+    }
+
     private final MotorController leftMotor;
     private final MotorController rightMotor;
-    private final double intakePower = -ConfigFileReader.instance.getDouble("gripper.intakePower").valueOr(0.3);
-    private final double outtakePower = ConfigFileReader.instance.getDouble("gripper.outtakePower").valueOr(0.1);
-    private final double idlePower = -ConfigFileReader.instance.getDouble("gripper.idlePower").valueOr(0.05);
+    private final double intakePower =
+            -ConfigFileReader.instance.getDouble("gripper.intakePower").valueOr(0.3);
+    private final double outtakePower =
+            ConfigFileReader.instance.getDouble("gripper.outtakePower").valueOr(0.1);
+    private final double idlePower =
+            -ConfigFileReader.instance.getDouble("gripper.idlePower").valueOr(0.05);
 
     public Gripper() {
         leftMotor = RobotProvider.instance.getMotor("gripper.leftMotor");
         rightMotor = RobotProvider.instance.getMotor("gripper.rightMotor");
     }
 
-    public void setMotorPower(final double power) {
-        checkContextOwnership();
+    @Override
+    protected GripperRequest getInitialRequest() {
+        return new Idle();
+    }
 
+    @Override
+    protected GripperRequest getIdleRequest() {
+        return new Idle();
+    }
+
+    @Override
+    protected GripperStatus run(GripperRequest request, boolean isRequestNew) {
+        switch (request) {
+            case Idle g -> {
+                setMotorPower(idlePower);
+            }
+            case Intake g -> {
+                setMotorPower(intakePower);
+            }
+            case Outtake g -> {
+                setMotorPower(outtakePower);
+            }
+        }
+        return new GripperStatus();
+    }
+
+    private void setMotorPower(final double power) {
         leftMotor.set(power);
         rightMotor.set(power);
-    }
-
-    public void intake() {
-        setMotorPower(intakePower);
-    }
-
-    public void outtake() {
-        setMotorPower(outtakePower);
-    }
-
-    public void idle() {
-        setMotorPower(idlePower);
     }
 }

src/main/java/com/team766/robot/burro_elevator/Robot.java
@@ -1,7 +1,7 @@
 package com.team766.robot.burro_elevator;
 
 import com.team766.framework.AutonomousMode;
-import com.team766.framework.Procedure;
+import com.team766.framework.RuleEngine;
 import com.team766.hal.RobotConfigurator;
 import com.team766.robot.burro_arm.mechanisms.Gripper;
 import com.team766.robot.burro_elevator.mechanisms.*;
@@ -9,9 +9,9 @@ import com.team766.robot.common.mechanisms.BurroDrive;
 
 public class Robot implements RobotConfigurator {
     // Declare mechanisms (as static fields) here
-    public static BurroDrive drive;
-    public static Elevator elevator;
-    public static Gripper gripper;
+    private BurroDrive drive;
+    private Elevator elevator;
+    private Gripper gripper;
 
     @Override
     public void initializeMechanisms() {
@@ -22,8 +22,13 @@ public class Robot implements RobotConfigurator {
     }
 
     @Override
-    public Procedure createOI() {
-        return new OI();
+    public RuleEngine createOI() {
+        return new OI(drive, elevator, gripper);
+    }
+
+    @Override
+    public RuleEngine createLights() {
+        return null;
     }
 
     @Override

src/main/java/com/team766/robot/burro_elevator/mechanisms/Elevator.java
@@ -1,13 +1,58 @@
 package com.team766.robot.burro_elevator.mechanisms;
 
+import static com.team766.framework.Conditions.checkForStatusWith;
+import static com.team766.framework.StatusBus.getStatusOrThrow;
+
 import com.team766.framework.Mechanism;
+import com.team766.framework.Request;
+import com.team766.framework.Status;
 import com.team766.hal.MotorController.ControlMode;
 import com.team766.hal.RobotProvider;
 import com.team766.hal.wpilib.CANSparkMaxMotorController;
 import com.team766.library.RateLimiter;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class Elevator extends Mechanism {
+public class Elevator extends Mechanism<Elevator.ElevatorRequest, Elevator.ElevatorStatus> {
+    public record ElevatorStatus(double position) implements Status {}
+
+    public sealed interface ElevatorRequest extends Request {}
+
+    public record SetPower(double power) implements ElevatorRequest {
+        @Override
+        public boolean isDone() {
+            return true;
+        }
+    }
+
+    public record SetPosition(double position) implements ElevatorRequest {
+        @Override
+        public boolean isDone() {
+            // TODO: also consider velocity
+            return checkForStatusWith(
+                    ElevatorStatus.class,
+                    s -> Math.abs(s.position - position) < POSITION_TOLERANCE);
+        }
+    }
+
+    public static ElevatorRequest makeHoldPosition() {
+        final double currentPosition = getStatusOrThrow(ElevatorStatus.class).position();
+        return new SetPosition(currentPosition);
+    }
+
+    public static ElevatorRequest makeNudgeUp() {
+        final double currentPosition = getStatusOrThrow(ElevatorStatus.class).position();
+        return new SetPosition(currentPosition + NUDGE_UP_INCREMENT);
+    }
+
+    public static ElevatorRequest makeNudgeDown() {
+        final double currentPosition = getStatusOrThrow(ElevatorStatus.class).position();
+        return new SetPosition(currentPosition - NUDGE_DOWN_INCREMENT);
+    }
+
+    private static final double NUDGE_UP_INCREMENT = 1.0; // inches
+    private static final double NUDGE_DOWN_INCREMENT = 1.0; // inches
+
+    private static final double POSITION_TOLERANCE = 0.5;
+
     private static final double MOTOR_ROTATIONS_TO_ELEVATOR_POSITION =
             (0.25 /*chain pitch = distance per tooth*/)
                     * (18. /*teeth per rotation of sprocket*/)
@@ -17,29 +62,31 @@ public class Elevator extends Mechanism {
     private final RateLimiter dashboardRateLimiter = new RateLimiter(0.1);
 
     public Elevator() {
-        motor = (CANSparkMaxMotorController)RobotProvider.instance.getMotor("elevator.Motor");
+        motor = (CANSparkMaxMotorController) RobotProvider.instance.getMotor("elevator.Motor");
         motor.setSmartCurrentLimit(10, 80, 200);
     }
 
-    public void setPower(final double power) {
-        checkContextOwnership();
-        motor.set(power);
-    }
-
-    public void setPosition(final double position) {
-        checkContextOwnership();
-
-        motor.set(ControlMode.Position, position / MOTOR_ROTATIONS_TO_ELEVATOR_POSITION);
-    }
-
-    public double getPosition() {
-        return motor.getSensorPosition() * MOTOR_ROTATIONS_TO_ELEVATOR_POSITION;
+    @Override
+    protected ElevatorRequest getInitialRequest() {
+        return new SetPower(0);
     }
 
     @Override
-    public void run() {
-        if (dashboardRateLimiter.next()) {
-            SmartDashboard.putNumber("[Elevator] Position", getPosition());
+    protected ElevatorRequest getIdleRequest() {
+        return makeHoldPosition();
+    }
+
+    @Override
+    protected ElevatorStatus run(ElevatorRequest request, boolean isRequestNew) {
+        switch (request) {
+            case SetPower g -> {
+                motor.set(g.power);
+            }
+            case SetPosition g -> {
+                motor.set(ControlMode.Position, g.position / MOTOR_ROTATIONS_TO_ELEVATOR_POSITION);
+            }
         }
+
+        return new ElevatorStatus(motor.getSensorPosition() * MOTOR_ROTATIONS_TO_ELEVATOR_POSITION);
     }
 }

src/main/java/com/team766/robot/common/mechanisms/BurroDrive.java
@@ -3,38 +3,73 @@ package com.team766.robot.common.mechanisms;
 import static com.team766.robot.common.constants.ConfigConstants.*;
 
 import com.team766.framework.Mechanism;
+import com.team766.framework.Request;
+import com.team766.framework.Status;
 import com.team766.hal.MotorController;
 import com.team766.hal.RobotProvider;
 import com.team766.logging.Category;
 
-public class BurroDrive extends Mechanism {
+public class BurroDrive extends Mechanism<BurroDrive.DriveRequest, BurroDrive.DriveStatus> {
+    public record DriveStatus() implements Status {}
 
-    private final MotorController leftMotor;
-    private final MotorController rightMotor;
+    public sealed interface DriveRequest extends Request {}
 
-    public BurroDrive() {
-        loggerCategory = Category.DRIVE;
-
-        leftMotor = RobotProvider.instance.getMotor(DRIVE_LEFT);
-        rightMotor = RobotProvider.instance.getMotor(DRIVE_RIGHT);
+    /*
+     * Stops each drive motor
+     */
+    public record Stop() implements DriveRequest {
+        @Override
+        public boolean isDone() {
+            return true;
+        }
     }
 
     /**
      * @param forward how much power to apply to moving the robot (positive being forward)
      * @param turn how much power to apply to turning the robot (positive being CCW)
      */
-    public void drive(double forward, double turn) {
-        checkContextOwnership();
-        leftMotor.set(forward - turn);
-        rightMotor.set(forward + turn);
+    public record ArcadeDrive(double forward, double turn) implements DriveRequest {
+        @Override
+        public boolean isDone() {
+            return true;
+        }
     }
 
-    /*
-     * Stops each drive motor
-     */
-    public void stopDrive() {
-        checkContextOwnership();
-        leftMotor.stopMotor();
-        rightMotor.stopMotor();
+    private final MotorController leftMotor;
+    private final MotorController rightMotor;
+
+    public BurroDrive() {
+        leftMotor = RobotProvider.instance.getMotor(DRIVE_LEFT);
+        rightMotor = RobotProvider.instance.getMotor(DRIVE_RIGHT);
+    }
+
+    @Override
+    public Category getLoggerCategory() {
+        return Category.DRIVE;
+    }
+
+    @Override
+    protected DriveRequest getInitialRequest() {
+        return new Stop();
+    }
+
+    @Override
+    protected DriveRequest getIdleRequest() {
+        return new Stop();
+    }
+
+    @Override
+    protected DriveStatus run(DriveRequest request, boolean isRequestNew) {
+        switch (request) {
+            case Stop g -> {
+                leftMotor.stopMotor();
+                rightMotor.stopMotor();
+            }
+            case ArcadeDrive g -> {
+                leftMotor.set(g.forward - g.turn);
+                rightMotor.set(g.forward + g.turn);
+            }
+        }
+        return new DriveStatus();
     }
 }

Team766/2025

src/main/java/com/team766/robot/burro_arm/Robot.java
@@ -1,16 +1,16 @@
 package com.team766.robot.burro_arm;
 
-import com.team766.framework.AutonomousMode;
-import com.team766.framework.Procedure;
-import com.team766.hal.RobotConfigurator;
+import com.team766.framework3.AutonomousMode;
+import com.team766.framework3.RuleEngine;
+import com.team766.hal.RobotConfigurator3;
 import com.team766.robot.burro_arm.mechanisms.*;
 import com.team766.robot.common.mechanisms.BurroDrive;
 
-public class Robot implements RobotConfigurator {
+public class Robot implements RobotConfigurator3 {
     // Declare mechanisms (as static fields) here
-    public static BurroDrive drive;
-    public static Arm arm;
-    public static Gripper gripper;
+    private BurroDrive drive;
+    private Arm arm;
+    private Gripper gripper;
 
     @Override
     public void initializeMechanisms() {
@@ -21,8 +21,13 @@ public class Robot implements RobotConfigurator {
     }
 
     @Override
-    public Procedure createOI() {
-        return new OI();
+    public RuleEngine createOI() {
+        return new OI(drive, arm, gripper);
+    }
+
+    @Override
+    public RuleEngine createLights() {
+        return null;
     }
 
     @Override

src/main/java/com/team766/robot/burro_arm/mechanisms/Gripper.java
@@ -1,38 +1,80 @@
 package com.team766.robot.burro_arm.mechanisms;
 
 import com.team766.config.ConfigFileReader;
-import com.team766.framework.Mechanism;
+import com.team766.framework3.Mechanism;
+import com.team766.framework3.Request;
+import com.team766.framework3.Status;
 import com.team766.hal.MotorController;
 import com.team766.hal.RobotProvider;
 
-public class Gripper extends Mechanism {
+public class Gripper extends Mechanism<Gripper.GripperRequest, Gripper.GripperStatus> {
+    public record GripperStatus() implements Status {}
+
+    public sealed interface GripperRequest extends Request {}
+
+    public record Idle() implements GripperRequest {
+        @Override
+        public boolean isDone() {
+            return true;
+        }
+    }
+
+    public record Intake() implements GripperRequest {
+        @Override
+        public boolean isDone() {
+            return true;
+        }
+    }
+
+    public record Outtake() implements GripperRequest {
+        @Override
+        public boolean isDone() {
+            return true;
+        }
+    }
+
     private final MotorController leftMotor;
     private final MotorController rightMotor;
-    private final double intakePower = -ConfigFileReader.instance.getDouble("gripper.intakePower").valueOr(0.3);
-    private final double outtakePower = ConfigFileReader.instance.getDouble("gripper.outtakePower").valueOr(0.1);
-    private final double idlePower = -ConfigFileReader.instance.getDouble("gripper.idlePower").valueOr(0.05);
+    private final double intakePower =
+            -ConfigFileReader.instance.getDouble("gripper.intakePower").valueOr(0.3);
+    private final double outtakePower =
+            ConfigFileReader.instance.getDouble("gripper.outtakePower").valueOr(0.1);
+    private final double idlePower =
+            -ConfigFileReader.instance.getDouble("gripper.idlePower").valueOr(0.05);
 
     public Gripper() {
         leftMotor = RobotProvider.instance.getMotor("gripper.leftMotor");
         rightMotor = RobotProvider.instance.getMotor("gripper.rightMotor");
     }
 
-    public void setMotorPower(final double power) {
-        checkContextOwnership();
+    @Override
+    protected GripperRequest getInitialRequest() {
+        return new Idle();
+    }
 
+    @Override
+    protected GripperRequest getIdleRequest() {
+        return new Idle();
+    }
+
+    @Override
+    protected GripperStatus run(GripperRequest request, boolean isRequestNew) {
+        switch (request) {
+            case Idle g -> {
+                setMotorPower(idlePower);
+            }
+            case Intake g -> {
+                setMotorPower(intakePower);
+            }
+            case Outtake g -> {
+                setMotorPower(outtakePower);
+            }
+        }
+        return new GripperStatus();
+    }
+
+    private void setMotorPower(final double power) {
         leftMotor.set(power);
         rightMotor.set(power);
-    }
-
-    public void intake() {
-        setMotorPower(intakePower);
-    }
-
-    public void outtake() {
-        setMotorPower(outtakePower);
-    }
-
-    public void idle() {
-        setMotorPower(idlePower);
     }
 }

src/main/java/com/team766/robot/burro_elevator/Robot.java
@@ -1,17 +1,17 @@
 package com.team766.robot.burro_elevator;
 
-import com.team766.framework.AutonomousMode;
-import com.team766.framework.Procedure;
-import com.team766.hal.RobotConfigurator;
+import com.team766.framework3.AutonomousMode;
+import com.team766.framework3.RuleEngine;
+import com.team766.hal.RobotConfigurator3;
 import com.team766.robot.burro_arm.mechanisms.Gripper;
 import com.team766.robot.burro_elevator.mechanisms.*;
 import com.team766.robot.common.mechanisms.BurroDrive;
 
-public class Robot implements RobotConfigurator {
+public class Robot implements RobotConfigurator3 {
     // Declare mechanisms (as static fields) here
-    public static BurroDrive drive;
-    public static Elevator elevator;
-    public static Gripper gripper;
+    private BurroDrive drive;
+    private Elevator elevator;
+    private Gripper gripper;
 
     @Override
     public void initializeMechanisms() {
@@ -22,8 +22,13 @@ public class Robot implements RobotConfigurator {
     }
 
     @Override
-    public Procedure createOI() {
-        return new OI();
+    public RuleEngine createOI() {
+        return new OI(drive, elevator, gripper);
+    }
+
+    @Override
+    public RuleEngine createLights() {
+        return new Lights();
     }
 
     @Override

src/main/java/com/team766/robot/burro_elevator/mechanisms/Elevator.java
@@ -1,13 +1,60 @@
 package com.team766.robot.burro_elevator.mechanisms;
 
-import com.team766.framework.Mechanism;
+import static com.team766.framework3.Conditions.checkForStatusWith;
+
+import com.team766.framework3.Mechanism;
+import com.team766.framework3.Request;
+import com.team766.framework3.Status;
 import com.team766.hal.MotorController.ControlMode;
 import com.team766.hal.RobotProvider;
 import com.team766.hal.wpilib.CANSparkMaxMotorController;
 import com.team766.library.RateLimiter;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class Elevator extends Mechanism {
+public class Elevator extends Mechanism<Elevator.ElevatorRequest, Elevator.ElevatorStatus> {
+    public static final double BOTTOM_POSITION = 100.0; // set this proper
+    public static final double TOP_POSITION = 0.0; // set this proper
+
+    public record ElevatorStatus(double position) implements Status {}
+
+    public sealed interface ElevatorRequest extends Request {}
+
+    public record SetPower(double power) implements ElevatorRequest {
+        @Override
+        public boolean isDone() {
+            return true;
+        }
+    }
+
+    public record SetPosition(double position) implements ElevatorRequest {
+        @Override
+        public boolean isDone() {
+            // TODO: also consider velocity
+            return checkForStatusWith(
+                    ElevatorStatus.class,
+                    s -> Math.abs(s.position - position) < POSITION_TOLERANCE);
+        }
+    }
+
+    public static ElevatorRequest makeHoldPosition() {
+        final double currentPosition = getStatusOrThrow(ElevatorStatus.class).position();
+        return new SetPosition(currentPosition);
+    }
+
+    public static ElevatorRequest makeNudgeUp() {
+        final double currentPosition = getStatusOrThrow(ElevatorStatus.class).position();
+        return new SetPosition(currentPosition + NUDGE_UP_INCREMENT);
+    }
+
+    public static ElevatorRequest makeNudgeDown() {
+        final double currentPosition = getStatusOrThrow(ElevatorStatus.class).position();
+        return new SetPosition(currentPosition - NUDGE_DOWN_INCREMENT);
+    }
+
+    private static final double NUDGE_UP_INCREMENT = 1.0; // inches
+    private static final double NUDGE_DOWN_INCREMENT = 1.0; // inches
+
+    private static final double POSITION_TOLERANCE = 0.5;
+
     private static final double MOTOR_ROTATIONS_TO_ELEVATOR_POSITION =
             (0.25 /*chain pitch = distance per tooth*/)
                     * (18. /*teeth per rotation of sprocket*/)
@@ -17,29 +64,31 @@ public class Elevator extends Mechanism {
     private final RateLimiter dashboardRateLimiter = new RateLimiter(0.1);
 
     public Elevator() {
-        motor = (CANSparkMaxMotorController)RobotProvider.instance.getMotor("elevator.Motor");
+        motor = (CANSparkMaxMotorController) RobotProvider.instance.getMotor("elevator.Motor");
         motor.setSmartCurrentLimit(10, 80, 200);
     }
 
-    public void setPower(final double power) {
-        checkContextOwnership();
-        motor.set(power);
-    }
-
-    public void setPosition(final double position) {
-        checkContextOwnership();
-
-        motor.set(ControlMode.Position, position / MOTOR_ROTATIONS_TO_ELEVATOR_POSITION);
-    }
-
-    public double getPosition() {
-        return motor.getSensorPosition() * MOTOR_ROTATIONS_TO_ELEVATOR_POSITION;
+    @Override
+    protected ElevatorRequest getInitialRequest() {
+        return new SetPower(0);
     }
 
     @Override
-    public void run() {
-        if (dashboardRateLimiter.next()) {
-            SmartDashboard.putNumber("[Elevator] Position", getPosition());
+    protected ElevatorRequest getIdleRequest() {
+        return makeHoldPosition();
+    }
+
+    @Override
+    protected ElevatorStatus run(ElevatorRequest request, boolean isRequestNew) {
+        switch (request) {
+            case SetPower g -> {
+                motor.set(g.power);
+            }
+            case SetPosition g -> {
+                motor.set(ControlMode.Position, g.position / MOTOR_ROTATIONS_TO_ELEVATOR_POSITION);
+            }
         }
+
+        return new ElevatorStatus(motor.getSensorPosition() * MOTOR_ROTATIONS_TO_ELEVATOR_POSITION);
     }
 }

src/main/java/com/team766/robot/common/mechanisms/BurroDrive.java
@@ -2,39 +2,74 @@ package com.team766.robot.common.mechanisms;
 
 import static com.team766.robot.common.constants.ConfigConstants.*;
 
-import com.team766.framework.Mechanism;
+import com.team766.framework3.Mechanism;
+import com.team766.framework3.Request;
+import com.team766.framework3.Status;
 import com.team766.hal.MotorController;
 import com.team766.hal.RobotProvider;
 import com.team766.logging.Category;
 
-public class BurroDrive extends Mechanism {
+public class BurroDrive extends Mechanism<BurroDrive.DriveRequest, BurroDrive.DriveStatus> {
+    public record DriveStatus() implements Status {}
 
-    private final MotorController leftMotor;
-    private final MotorController rightMotor;
+    public sealed interface DriveRequest extends Request {}
 
-    public BurroDrive() {
-        loggerCategory = Category.DRIVE;
-
-        leftMotor = RobotProvider.instance.getMotor(DRIVE_LEFT);
-        rightMotor = RobotProvider.instance.getMotor(DRIVE_RIGHT);
+    /*
+     * Stops each drive motor
+     */
+    public record Stop() implements DriveRequest {
+        @Override
+        public boolean isDone() {
+            return true;
+        }
     }
 
     /**
      * @param forward how much power to apply to moving the robot (positive being forward)
      * @param turn how much power to apply to turning the robot (positive being CCW)
      */
-    public void drive(double forward, double turn) {
-        checkContextOwnership();
-        leftMotor.set(forward - turn);
-        rightMotor.set(forward + turn);
+    public record ArcadeDrive(double forward, double turn) implements DriveRequest {
+        @Override
+        public boolean isDone() {
+            return true;
+        }
     }
 
-    /*
-     * Stops each drive motor
-     */
-    public void stopDrive() {
-        checkContextOwnership();
-        leftMotor.stopMotor();
-        rightMotor.stopMotor();
+    private final MotorController leftMotor;
+    private final MotorController rightMotor;
+
+    public BurroDrive() {
+        leftMotor = RobotProvider.instance.getMotor(DRIVE_LEFT);
+        rightMotor = RobotProvider.instance.getMotor(DRIVE_RIGHT);
+    }
+
+    @Override
+    public Category getLoggerCategory() {
+        return Category.DRIVE;
+    }
+
+    @Override
+    protected DriveRequest getInitialRequest() {
+        return new Stop();
+    }
+
+    @Override
+    protected DriveRequest getIdleRequest() {
+        return new Stop();
+    }
+
+    @Override
+    protected DriveStatus run(DriveRequest request, boolean isRequestNew) {
+        switch (request) {
+            case Stop g -> {
+                leftMotor.stopMotor();
+                rightMotor.stopMotor();
+            }
+            case ArcadeDrive g -> {
+                leftMotor.set(g.forward - g.turn);
+                rightMotor.set(g.forward + g.turn);
+            }
+        }
+        return new DriveStatus();
     }
 }