f5a53782aefe8a829b786989f3983c69fe9c28c3
Max Spier <68516760+maxspier@users.noreply.github.com>
2025-06-25 18:16:21 -0700

    Lint

--- src/main/java/com/team766/robot/reva_2025/mechanisms/Quest.java
+++ src/main/java/com/team766/robot/reva_2025/mechanisms/Quest.java
@@ -7,8 +7,6 @@ import com.team766.quest.QuestNav;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
-import edu.wpi.first.units.AngleUnit;
-import edu.wpi.first.units.measure.Angle;
 
 public class Quest extends MechanismWithStatus<Quest.QuestStatus> {
 
@@ -21,23 +19,29 @@ public class Quest extends MechanismWithStatus<Quest.QuestStatus> {
 
     // First, Declare our geometrical transform from the Quest to the robot center
 
-    private static double x_offset = ConfigFileReader.instance.getDouble("quest.x_offset").valueOr(0d);
-    private static double y_offset = ConfigFileReader.instance.getDouble("quest.y_offset").valueOr(0d);
-    private static double rotation = ConfigFileReader.instance.getDouble("quest.rotation_degrees").valueOr(0d);
-    
-    private static double x_field_translation = ConfigFileReader.instance.getDouble("quest.x_translation").valueOr(0d);
-    private static double y_field_translastion = ConfigFileReader.instance.getDouble("quest.y_translation").valueOr(0d);
+    private static double x_offset =
+            ConfigFileReader.instance.getDouble("quest.x_offset").valueOr(0d);
+    private static double y_offset =
+            ConfigFileReader.instance.getDouble("quest.y_offset").valueOr(0d);
+    private static double rotation =
+            ConfigFileReader.instance.getDouble("quest.rotation_degrees").valueOr(0d);
+
+    private static double x_field_translation =
+            ConfigFileReader.instance.getDouble("quest.x_translation").valueOr(0d);
+    private static double y_field_translastion =
+            ConfigFileReader.instance.getDouble("quest.y_translation").valueOr(0d);
 
     private static Transform2d QUEST_TO_ROBOT =
             new Transform2d(x_offset, y_offset, Rotation2d.fromDegrees(rotation));
 
-    private static Pose2d robotPose = new Pose2d(x_field_translation, y_field_translastion, new Rotation2d());
+    private static Pose2d robotPose =
+            new Pose2d(x_field_translation, y_field_translastion, new Rotation2d());
 
     private static Pose2d questPose = robotPose.transformBy(QUEST_TO_ROBOT);
 
     public record QuestStatus(Pose2d currentPose) implements Status {
         public Pose2d getPose() {
-            
+
             return currentPose;
         }
     }
@@ -45,10 +49,10 @@ public class Quest extends MechanismWithStatus<Quest.QuestStatus> {
     public Quest() {
         questNav = new QuestNav();
         questNav.setPose(questPose);
-        //resetQuestPosition();
+        // resetQuestPosition();
     }
 
-    public void resetQuestPosition(){
+    public void resetQuestPosition() {
         questNav.setPose(questPose);
     }
 
