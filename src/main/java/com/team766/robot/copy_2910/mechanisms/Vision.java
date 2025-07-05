a14141adcc714201e85e525eceba9891641835cf
Max Spier <68516760+maxspier@users.noreply.github.com>
2025-07-04 19:43:04 -0700

    create vision code for 2910 robot (#95)
    ## Description
    
    Vision code for basic estimator of pose. Still some TODO's in here for
    the rest of the stuff to fill out once the camera is mounted and we know
    the field
    
    
    ## How Has This Been Tested?
    
    No tests yet but I followed the documentation very closely as I made
    this
    
    ---------
    
    Co-authored-by: Max Spier <68516760+Mixmix00@users.noreply.github.com>

---
+++ src/main/java/com/team766/robot/copy_2910/mechanisms/Vision.java
@@ -0,0 +1,67 @@
+package com.team766.robot.copy_2910.mechanisms;
+
+import com.team766.config.ConfigFileReader;
+import com.team766.framework.MechanismWithStatus;
+import com.team766.framework.Status;
+import com.team766.orin.GetOrinRawValue;
+import com.team766.orin.ValueNotFoundOnTableError;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+
+public class Vision extends MechanismWithStatus<Vision.VisionStatus> {
+
+    private static double leftScoringPositionX =
+            ConfigFileReader.instance.getDouble("vision.leftScoringPositionX").valueOr(0d);
+
+    private static double leftScoringPositionY =
+            ConfigFileReader.instance.getDouble("vision.leftScoringPositionY").valueOr(0d);
+
+    private static double rightScoringPositionX =
+            ConfigFileReader.instance.getDouble("vision.rightScoringPositionX").valueOr(0d);
+
+    private static double rightScoringPositionY =
+            ConfigFileReader.instance.getDouble("vision.rightScoringPositionY").valueOr(0d);
+
+    private GetOrinRawValue camera = new GetOrinRawValue("MainCamera", 0);
+
+    public static record VisionStatus(int ID, double x, double y, boolean isValid)
+            implements Status {
+
+        public Pose2d getApriltagPose2d() throws Exception {
+
+            if (!isValid) {
+                throw new Exception("Pose is not valid");
+            }
+            return new Pose2d(x, y, new Rotation2d());
+        }
+    }
+
+    public Vision() {}
+
+    public static Pose2d getTargetPositionLeft() {
+        return new Pose2d(leftScoringPositionX, leftScoringPositionY, new Rotation2d());
+    }
+
+    public static Pose2d getTargetPositionRight() {
+        return new Pose2d(rightScoringPositionX, rightScoringPositionY, new Rotation2d());
+    }
+
+    @Override
+    protected VisionStatus updateStatus() {
+        double[] poseData;
+        try {
+            poseData = camera.getRawPoseData();
+        } catch (ValueNotFoundOnTableError e) {
+            log("No pose data found on table for camera: " + e.getMessage());
+            return new VisionStatus(0, 0, 0, false); // Return a default status if no data is found
+        }
+
+        /*
+         * Can assume there will be only one tag as the tag here should always be the one on the reef\
+         * poseData[0] is the collect time - can ignore for this situation
+         */
+        // AprilTag tag = new AprilTag((int) poseData[1], new Pose3d(poseData[2], poseData[3],
+        // poseData[4], new Rotation3d()));
+        return new VisionStatus((int) poseData[1], poseData[2], poseData[3], true);
+    }
+}
---
+++ src/main/java/com/team766/robot/copy_2910/procedures/AutoAlign.java
@@ -0,0 +1,79 @@
+package com.team766.robot.copy_2910.procedures;
+
+import com.team766.controllers.PIDController;
+import com.team766.framework.Context;
+import com.team766.framework.Procedure;
+import com.team766.robot.common.mechanisms.SwerveDrive;
+import com.team766.robot.copy_2910.mechanisms.Vision;
+import com.team766.robot.reva_2025.constants.ConfigConstants;
+import edu.wpi.first.math.geometry.Pose2d;
+
+public class AutoAlign extends Procedure {
+    private Pose2d targetPosition;
+    private SwerveDrive drive;
+    private PIDController pidControllerX;
+    private PIDController pidControllerY;
+    private PIDController pidControllerRotation;
+
+    public AutoAlign(Pose2d targetPosition, SwerveDrive drive) {
+        this.targetPosition = targetPosition;
+        this.drive = reserve(drive);
+        pidControllerX = PIDController.loadFromConfig(ConfigConstants.DRIVE_TARGET_TRANSLATION_PID);
+        pidControllerY = PIDController.loadFromConfig(ConfigConstants.DRIVE_TARGET_TRANSLATION_PID);
+        pidControllerRotation =
+                PIDController.loadFromConfig(ConfigConstants.DRIVE_TARGET_ROTATION_PID);
+    }
+
+    public AutoAlign(Pose2d targetPosition, double threshold, SwerveDrive drive) {
+        this(targetPosition, drive);
+        pidControllerX.setThreshold(threshold);
+        pidControllerY.setThreshold(threshold);
+    }
+
+    public void run(Context context) {
+        Pose2d currentPosition;
+        try {
+            currentPosition = getStatusOrThrow(Vision.VisionStatus.class).getApriltagPose2d();
+        } catch (Exception e) {
+            drive.stopDrive();
+            log("No valid pose found from vision: " + e.getMessage());
+            return; // Exit if no valid pose is found
+        }
+        // currentHeading = getStatusOrThrow(SwerveDrive.DriveStatus.class).heading()
+        double currentHeading = 0;
+
+        pidControllerX.setSetpoint(targetPosition.getX());
+        pidControllerY.setSetpoint(targetPosition.getY());
+        double correctedTargetDegrees =
+                targetPosition.getRotation().getDegrees()
+                        + 360
+                                * Math.round(
+                                        (currentHeading - targetPosition.getRotation().getDegrees())
+                                                / 360);
+        pidControllerRotation.setSetpoint(correctedTargetDegrees);
+        while (!pidControllerX.isDone()
+                || !pidControllerY.isDone()
+                || !pidControllerRotation.isDone()) {
+            try {
+                currentPosition = getStatusOrThrow(Vision.VisionStatus.class).getApriltagPose2d();
+            } catch (Exception e) {
+                log("No valid pose found from vision: " + e.getMessage());
+                drive.stopDrive();
+                return; // Exit if no valid pose is found
+            }
+            // currentHeading = getStatusOrThrow(SwerveDrive.DriveStatus.class).heading()
+            currentHeading = 0;
+
+            pidControllerX.calculate(currentPosition.getX());
+            pidControllerY.calculate(currentPosition.getY());
+            pidControllerRotation.calculate(currentHeading);
+
+            drive.controlFieldOriented(
+                    pidControllerX.isDone() ? 0 : pidControllerX.getOutput(),
+                    pidControllerY.isDone() ? 0 : pidControllerY.getOutput(),
+                    pidControllerRotation.isDone() ? 0 : pidControllerRotation.getOutput());
+            context.yield();
+        }
+        drive.stopDrive();
+    }
+}
