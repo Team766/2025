edf0e1fb188a34040049fc0231f6f27bb6aeed2c
Max Spier <68516760+Mixmix00@users.noreply.github.com>
2025-07-12 15:42:28 -0700

    Refactor OuttakeCoral formatting and imports
    Cleaned up import order, removed unnecessary blank lines, and improved code formatting for readability in OuttakeCoral.java. No functional changes were made.

--- src/main/java/com/team766/robot/copy_2910/procedures/OuttakeCoral.java
+++ src/main/java/com/team766/robot/copy_2910/procedures/OuttakeCoral.java
@@ -1,31 +1,28 @@
 package com.team766.robot.copy_2910.procedures;
 
-import java.util.Optional;
 import com.team766.framework.Context;
 import com.team766.framework.Procedure;
 import com.team766.robot.copy_2910.mechanisms.Intake;
+import java.util.Optional;
 
 public class OuttakeCoral extends Procedure {
 
     private Intake intake;
+
     public OuttakeCoral() {
         intake = reserve(intake);
     }
-
 
     @Override
     public void run(Context context) {
         Optional<Intake.IntakeStatus> status = getStatus(Intake.IntakeStatus.class);
 
-        while(status.get().hasCoralInFrontCenter()){
+        while (status.get().hasCoralInFrontCenter()) {
             status = getStatus(Intake.IntakeStatus.class);
-            
+
             intake.turnRightPositive();
             intake.turnLeftNegative();
-            
-        
         }
         intake.stop();
     }
-    
 }
