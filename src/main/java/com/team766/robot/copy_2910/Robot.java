412da194c48d072b0995169da286d7eb987f9bda
Max Spier <68516760+maxspier@users.noreply.github.com>
2025-07-23 19:28:30 -0500

    Add Climber mechanism and update Robot initialization (#103)

--- src/main/java/com/team766/robot/copy_2910/Robot.java
+++ src/main/java/com/team766/robot/copy_2910/Robot.java
@@ -3,20 +3,35 @@ package com.team766.robot.copy_2910;
 import com.team766.framework.AutonomousMode;
 import com.team766.framework.RuleEngine;
 import com.team766.hal.RobotConfigurator;
+import com.team766.robot.burro_elevator.mechanisms.Elevator;
 import com.team766.robot.common.SwerveConfig;
 import com.team766.robot.common.mechanisms.SwerveDrive;
+import com.team766.robot.copy_2910.mechanisms.Climber;
 import com.team766.robot.copy_2910.mechanisms.Intake;
+import com.team766.robot.copy_2910.mechanisms.Shoulder;
+import com.team766.robot.copy_2910.mechanisms.Vision;
+import com.team766.robot.copy_2910.mechanisms.Wrist;
 
 public class Robot implements RobotConfigurator {
 
     private SwerveDrive drive;
     private Intake intake;
+    private Climber climber;
+    private Elevator elevator;
+    private Vision vision;
+    private Shoulder shoulder;
+    private Wrist wrist;
 
     @Override
     public void initializeMechanisms() {
         SwerveConfig swerveConfig = new SwerveConfig();
         drive = new SwerveDrive(swerveConfig);
         intake = new Intake();
+        climber = new Climber();
+        elevator = new Elevator();
+        vision = new Vision();
+        shoulder = new Shoulder();
+        wrist = new Wrist();
     }
 
     @Override
