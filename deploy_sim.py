a023e06ee6da025f91403bcf4cb5758c1f8dbc13
Ryan Cahoon <ryan.m.cahoon@gmail.com>
2025-09-19 08:33:48 +0000

    run the simulator in a screen session

--- deploy_sim.py
+++ deploy_sim.py
@@ -1,72 +1,20 @@
 #!/usr/bin/env python3
 
-import ctypes
-import ctypes.util
 import os
 import shutil
-import signal
 import socket
 import subprocess
-import threading
 import time
-
-# Constant taken from http://linux.die.net/include/linux/prctl.h
-PR_SET_PDEATHSIG = 1
-
-class PrCtlError(Exception):
-    pass
-
-def set_parent_exit_signal():
-    """
-    This uses a Linux-specific feature to configure the child process to receive a SIGTERM signal
-    when the parent process (this script) dies. This ensures we don't leave any orphaned processes.
-    """
-    # http://linux.die.net/man/2/prctl
-    libc = ctypes.CDLL(ctypes.util.find_library("c"), use_errno=True)
-    if libc.prctl(PR_SET_PDEATHSIG, signal.SIGTERM) != 0:
-        errno = ctypes.get_errno()
-        raise OSError(errno, f"SET_PDEATHSIG prctl failed: {os.strerror(errno)}")
-
-class TerminatingPopen(subprocess.Popen):
-    def __exit__(self, exc_type, value, traceback) -> None:
-        self.terminate()
-        return super().__exit__(exc_type, value, traceback)
-
-connected = None
-
-def code_monitor_thread():
-    """
-    This is run in a daemon thread to monitor and report the state of the robot code to VS Code.
-    It determines whether the robot code is running by polling whether it can connect to the HTTP
-    server on port 5800 that's built into the robot code.
-    """
-    global connected
-    iter = 0
-    while True:
-        try:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                s.connect(("localhost", 5800))
-                if connected != True:
-                    # If you change this print, also update the regex in .vscode/tasks.json
-                    print("Robot code started")
-                    connected = True
-                iter = 0
-        except ConnectionRefusedError:
-            if connected != False:
-                # If you change this print, also update the regex in .vscode/tasks.json
-                print("Waiting for robot code to start")
-                connected = False
-            # Display a simple animation to show signs of life while we're waiting for the
-            # robot code to start.
-            iter += 1
-            print("." * (iter % 6) + "      ", end="\r")
-        time.sleep(1)
 
 def main(argv=None):
     # Handle the case where somebody tried to run this script from somewhere besides the root
     # directory of the repo.
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     project_base = os.getcwd()
+
+    # Install dependencies
+    subprocess.check_call(["sudo", "apt", "update"])
+    subprocess.check_call(["sudo", "apt", "install", "-y", "screen"])
 
     # Create a directory in which to keep all of the simulator's files.
     os.makedirs("build-sim", exist_ok=True)
@@ -123,19 +71,38 @@ def main(argv=None):
 
     os.chdir(extracted_dir)
 
+    # If you change this print, also update the regex in .vscode/tasks.json
+    print("Waiting for robot code to start")
+
     # Wait until the robot code has started before starting the simulator.
+    # It determines whether the robot code is running by polling whether it can connect to the HTTP
+    # server on port 5800 that's built into the robot code.
     # This is because the simulator's run script prints the link to the viewer webpage, but we don't
     # want the user opening that until the robot code has actually started running (otherwise, the
     # right-side pane that tries to display data from the HTTP server built into the robot code will
     # fail to load).
-    threading.Thread(target=code_monitor_thread, daemon=True).start()
-
-    while not connected:
-        time.sleep(1)
+    iter = 0
+    while True:
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.connect(("localhost", 5800))
+                break
+        except ConnectionRefusedError:
+            # Display a simple animation to show signs of life while we're waiting for the
+            # robot code to start.
+            iter += 1
+            print("." * (iter % 6) + "      ", end="\r")
+            time.sleep(1)
 
     # Invoke the simulator package's entrypoint.
-    with TerminatingPopen(["./run.sh", project_base, f"{project_base}/build-sim"]) as p:
-        p.wait()
+    # NOTE(2025-09-19): There appears to be some issue in Codespaces right now that causes
+    # the Run Simulator task to randomly restart (sometimes accompanied by the message
+    # "Extension Host Process exited with code: null, signal: SIGTERM"). Thus, we run the simulator
+    # in a GNU Screen session so that if the task restarts, we can just reconnect to the existing
+    # instance of the simulator without any disruption to the user.
+    os.execlp(
+        "screen", "screen", "-D", "-R", "-S", "simulator",
+        "./run.sh", project_base, f"{project_base}/build-sim")
 
 if __name__ == "__main__":
     try:
