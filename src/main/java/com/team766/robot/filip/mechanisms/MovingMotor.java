e8f24570c03a63bb64be8f505e0631e17620bf22
filip-dob <filip.dobrzycki@gmail.com>
2025-10-02 20:03:16 -0700

    Update MovingMotor.java

--- src/main/java/com/team766/robot/filip/mechanisms/MovingMotor.java
+++ src/main/java/com/team766/robot/filip/mechanisms/MovingMotor.java
@@ -1,1 +1,9 @@
-
+public class FilipMovingMotor extends MechanismWithStatus<MovingMotor.MovingMotorStatus> {
+  public record MovingMotorStatus(doublecurrentPosition) implements Status {}
+  protected MovingMotorStatus updateStatus() {
+    return 4
+  }
+  RobotProvider.instance.getMotor()
+  public Motor() {}
+  
+}
