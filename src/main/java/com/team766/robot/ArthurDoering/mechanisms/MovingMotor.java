4fff9853775e920bda2cbba13b52777044dbeb57
team766-laptop19 <admin@team766.com>
2025-10-08 18:44:19 -0700

    Update MovingMotor.java

--- src/main/java/com/team766/robot/ArthurDoering/mechanisms/MovingMotor.java
+++ src/main/java/com/team766/robot/ArthurDoering/mechanisms/MovingMotor.java
@@ -1,8 +1,25 @@
+package com.team766.robot.ArthurDoering.mechanisms;
+
 import com.team766.framework.MechanismWithStatus;
 import com.team766.framework.Status;
-import com.team766.simulator.elements.MotorController;
+import com.team766.hal.MotorController;
+import com.team766.hal.RobotProvider;
 
 public class MovingMotor extends MechanismWithStatus<MovingMotor.MovingMotorStatus> {
-    MotorController motor = RobotProvider.instance.getMotor("LeftMotor"):
-    public record MovingMotorStatus(double currentPosition) implements Status {}
+
+    public MovingMotor(){
+    }
+
+    MotorController motor = RobotProvider.instance.getMotor(configName:"motor");
+
+    public record MovingMotorStatus(double currentPosition) implements Status{
+    }
+
+    public void setMotorPower(double power){
+        motor.set(power);
+    }
+
+    protected MovingMotorStatus updateStatus(){
+        return new MovingMotorStatus(0);
+    }
 }
