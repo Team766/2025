f6bdc76b195becfdfc049c196a0a954d1142084e
filip-dob <filip.dobrzycki@gmail.com>
2025-10-18 14:28:38 -0700

    implemented procedure

--- src/main/java/com/team766/robot/filip/procedures/MoveMotor.java
+++ src/main/java/com/team766/robot/filip/procedures/MoveMotor.java
@@ -1,7 +1,17 @@
+package com.team766.robot.filip.procedures;
+import com.team766.framework.Context;
+import com.team766.framework.Procedure;
+import com.team766.robot.filip.mechanisms.FilipMovingMotor;
+
 public class MoveMotor extends Procedure {
   private FilipMovingMotor motor;
-  public MoveMotor(MovingMotor myMotor){
+  public MoveMotor(FilipMovingMotor myMotor){
     motor = reserve(myMotor);
   }
-
+  @Override
+  public void run(Context context) {
+    motor.moveSpeed(1);
+    context.waitForSeconds(5);
+    motor.moveSpeed(0);
+  }
 }
