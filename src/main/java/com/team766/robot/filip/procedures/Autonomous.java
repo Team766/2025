13b93c44d906707509869da4ea75fa835076d8bc
filip-dob <100043837+qntmcube@users.noreply.github.com>
2025-10-22 18:57:54 -0700

    commited oi and tank drive

---
+++ src/main/java/com/team766/robot/filip/procedures/Autonomous.java
@@ -0,0 +1,29 @@
+package com.team766.robot.filip.procedures;
+
+import com.team766.framework.Context;
+import com.team766.framework.Procedure;
+import com.team766.robot.filip.mechanisms.Drive;
+import com.team766.robot.filip.mechanisms.FilipMovingMotor;
+
+public class Autonomous extends Procedure {
+    private FilipMovingMotor motor;
+    private Drive drive;
+    public Autonomous(Drive myDrive, FilipMovingMotor myMotor){
+        drive = reserve(myDrive);
+        motor = reserve(myMotor);
+    }
+    public void run(Context context) {
+        drive.move_left(1);
+        drive.move_right(1);
+        
+        context.waitForSeconds(5);
+
+        drive.move_left(0);
+        drive.move_right(0);
+
+        motor.moveSpeed(1);
+        context.waitForSeconds(3);
+        motor.moveSpeed(0);
+    }
+    
+}
