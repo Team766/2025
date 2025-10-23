e0e35a02b224eb97a5603c78abd385cccc8c3141
dejabot <dg@dejabotz.com>
2025-10-22 19:42:43 -0700

    update button mapping for xbox-based driver controls

--- src/main/java/com/team766/robot/outlaw/bearbot/constants/InputConstants.java
+++ src/main/java/com/team766/robot/outlaw/bearbot/constants/InputConstants.java
@@ -11,11 +11,11 @@ public final class InputConstants {
     // Axes
     public static final int LEFTSTICK_AXIS_LEFT_RIGHT = 0;
     public static final int LEFTSTICK_AXIS_FORWARD_BACKWARD = 1;
-    public static final int RIGHTSTICK_AXIS_LEFT_RIGHT = 4; // double-check this
-    public static final int RIGHTSTICK_AXIS_FORWARD_BACKWARD = 5; // double-check this
+    public static final int RIGHTSTICK_AXIS_LEFT_RIGHT = 4;
+    public static final int RIGHTSTICK_AXIS_FORWARD_BACKWARD = 5;
 
     // Buttons
-    public static final int BUTTON_RESET_GYRO = 7; // Start button
-    public static final int BUTTON_RESET_POS = 8; // Back button
+    public static final int BUTTON_RESET_GYRO = 6; // Start button
+    public static final int BUTTON_RESET_POS = 7; // Back button
     public static final int BUTTON_FINE_DRIVING = 5; // Right bumper
 }
