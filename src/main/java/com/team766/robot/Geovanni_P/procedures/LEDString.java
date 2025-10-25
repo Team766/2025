6525fb5e0f7cbb54b871b14c59c1f3908f37b3f0
MAXSPIER <mspier00001@gmail.com>
2025-10-25 16:32:57 -0700

    CANdle Geo

---
+++ src/main/java/com/team766/robot/Geovanni_P/procedures/LEDString.java
@@ -0,0 +1,43 @@
+package com.team766.robot.Geovanni_P.procedures;
+
+import com.team766.framework.RuleEngine;
+import com.team766.robot.Geovanni_P.OI;
+
+public class LEDString {
+
+    public LEDString(String configPrefix) {
+            //TODO Auto-generated constructor stub
+        }
+    
+        public class Lights extends LEDString {
+
+        public Lights(String configPrefix) {
+            super(configPrefix);
+        }
+    
+        public class OI extends RuleEngine {
+
+            public OI()
+        }
+    
+    
+    
+    
+    
+    
+    
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
