4e5e37cabeba58b85893b924fa9a1a59c0df7412
kavusikevan-dot <kavusikevan@gmail.com>
2025-10-25 19:14:40 -0700

    axs

--- src/main/java/com/team766/robot/Kevan/procedures/Autonomous.java
+++ src/main/java/com/team766/robot/Kevan/procedures/Autonomous.java
@@ -14,13 +14,13 @@ public class Autonomous extends Procedure {
         shooter = reserve(myShooter);
     }
 
-    public void AutonPro(Context context) {
+    public void run(Context context) {
         drive.move_straight(1);
         context.waitForSeconds(4);
         drive.move_straight(0);
         shooter.SetShooterSpeed(0.5);
         context.waitForSeconds(2);
         shooter.SetShooterSpeed(0);
-
     }
+
 }
