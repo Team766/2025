676f8f23fcc68218e7ec401b703adb7777cec85b
Mark Wilson <MW0901@proton.me>
2025-10-29 18:38:09 -0700

    Capstone Project

---
+++ src/main/java/com/team766/robot/mark/CANdle/CapstoneLights.java
@@ -0,0 +1,40 @@
+package com.team766.robot.mark.CANdle;
+import java.util.Set;
+import static com.team766.framework.RulePersistence.*;
+
+import com.team766.framework.RuleGroup;
+import com.team766.hal.JoystickReader;
+import com.team766.hal.RobotProvider;
+import com.team766.robot.common.mechanisms.LEDString;
+import com.team766.robot.common.constants.ColorConstants;
+import com.ctre.phoenix.led.Animation;
+import com.ctre.phoenix.led.RainbowAnimation;
+import com.team766.robot.common.constants.InputConstants;
+
+public class CapstoneLights extends RuleGroup {
+    private final LEDString ledString  = new LEDString("LEDs");
+    private final LEDString.Segment ledStringSegment = ledString.makeSegment(0, 7);
+    
+    public CapstoneLights() {
+        final Animation rainbowAnim = new RainbowAnimation();
+        final JoystickReader gamepad = RobotProvider.instance.getJoystick(1);
+
+        addRule("Yellow Lights for Cone",
+                gamepad.whenButton(InputConstants.GAMEPAD_DPAD_LEFT),
+                ONCE_AND_HOLD,
+                Set.of(ledString),
+                () -> {ledStringSegment.setColor(ColorConstants.YELLOW);});
+
+        addRule("Purple Lights for Cube",
+                gamepad.whenButton(InputConstants.GAMEPAD_DPAD_DOWN),
+                ONCE_AND_HOLD,
+                Set.of(ledString),
+                () -> {ledStringSegment.setColor(ColorConstants.PURPLE);});
+
+        addRule("Rainbow Lights for Defense",
+                gamepad.whenButton(InputConstants.GAMEPAD_DPAD_UP),
+                ONCE_AND_HOLD,
+                Set.of(ledString),
+                () -> {ledStringSegment.animate(rainbowAnim);});
+    }
+}
---
+++ src/main/java/com/team766/robot/mark/mechanisms/LEDString.java
@@ -0,0 +1,145 @@
+package com.team766.robot.mark.mechanisms;
+
+import com.ctre.phoenix.ErrorCode;
+import com.ctre.phoenix.led.Animation;
+import com.ctre.phoenix.led.CANdle;
+import com.team766.config.ConfigFileReader;
+import com.team766.framework.MultiFacetedMechanism;
+import com.team766.framework.NoReservationRequired;
+import com.team766.library.ValueProvider;
+import com.team766.logging.Logger;
+import com.team766.logging.Severity;
+import com.team766.math.Maths;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.util.Color8Bit;
+import java.util.ArrayList;
+import com.team766.logging.Category;
+
+public class LEDString extends MultiFacetedMechanism {
+    private final CANdle candle;
+    private final ArrayList<Segment> segments = new ArrayList<>();
+    private final boolean[] activeAnimations;
+
+    public LEDString(String configPrefix) {
+        final ValueProvider<Integer> deviceId =
+                ConfigFileReader.getInstance().getInt(configPrefix + ".deviceId");
+        final ValueProvider<String> canBus =
+                ConfigFileReader.getInstance().getString(configPrefix + ".CANBus");
+        if (deviceId.hasValue()) {
+            candle = new CANdle(deviceId.get(), canBus.valueOr(""));
+            activeAnimations = new boolean[candle.getMaxSimultaneousAnimationCount()];
+        } else {
+            Logger.get(Category.CONFIGURATION)
+                    .logRaw(
+                            Severity.ERROR,
+                            "Error getting configuration for LEDString "
+                                    + configPrefix
+                                    + " from config file. No lights will be shown.");
+            candle = null;
+            activeAnimations = new boolean[0];
+        }
+    }
+
+    @Override
+    public Category getLoggerCategory() {
+        return Category.LIGHTS;
+    }
+
+    private void handleError(ErrorCode e) {
+        if (!e.equals(ErrorCode.OK)) {
+            log(Severity.ERROR, "Error setting LED segment " + e);
+        }
+    }
+
+    private int reserveAnimation() {
+        for (int i = 0; i < activeAnimations.length; ++i) {
+            if (activeAnimations[i] == false) {
+                activeAnimations[i] = true;
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private void releaseAnimation(int animationIndex) {
+        if (candle == null) {
+            return;
+        }
+        handleError(candle.clearAnimation(animationIndex));
+        activeAnimations[animationIndex] = false;
+    }
+
+    public class Segment extends MechanismFacet {
+        private final int startIndex;
+        private final int count;
+        private int animationIndex = -1;
+
+        private Segment(int startIndex, int count) {
+            this.startIndex = startIndex;
+            this.count = count;
+        }
+
+        @Override
+        public Category getLoggerCategory() {
+            return Category.LIGHTS;
+        }
+
+        @Override
+        public void onMechanismIdle() {
+            setColor(0, 0, 0);
+        }
+
+        public void setColor(int r, int g, int b) {
+            if (candle == null) {
+                return;
+            }
+            handleError(candle.setLEDs(r, g, b, 0, startIndex, count));
+            if (animationIndex != -1) {
+                releaseAnimation(animationIndex);
+            }
+        }
+
+        public void setColor(Color color) {
+            var color8 = new Color8Bit(color);
+            setColor(color8.red, color8.green, color8.blue);
+        }
+
+        public void animate(Animation animation) {
+            if (candle == null) {
+                return;
+            }
+            if (animationIndex == -1) {
+                animationIndex = reserveAnimation();
+                if (animationIndex == -1) {
+                    log(
+                            Severity.ERROR,
+                            "No more available animations. CANdle only supports "
+                                    + activeAnimations.length
+                                    + " simultaneous animations.");
+                    return;
+                }
+            }
+            animation.setLedOffset(startIndex);
+            animation.setNumLed(count);
+            handleError(candle.animate(animation, animationIndex));
+        }
+    }
+
+    @NoReservationRequired
+    public Segment makeSegment(int ledStartIndex, int ledCount) {
+        final int ledEndIndex = ledStartIndex + ledCount;
+        for (var s : segments) {
+            if (Maths.overlaps(ledStartIndex, ledEndIndex, s.startIndex, s.startIndex + s.count)) {
+                throw new IllegalArgumentException(
+                        "Range of LEDs overlaps with an existing segment");
+            }
+        }
+        var segment = new Segment(ledStartIndex, ledCount);
+        addFacet(segment);
+        segments.add(segment);
+        return segment;
+    }
+
+    @Override
+    protected void run() {}
+}
