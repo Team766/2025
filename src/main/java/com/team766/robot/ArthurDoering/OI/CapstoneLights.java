efcf189646205635d0836e3be2056d3bc7e460af
team766-laptop19 <admin@team766.com>
2025-11-01 13:28:28 -0700

    Capstone cleanup

--- src/main/java/com/team766/robot/ArthurDoering/OI/CapstoneLights.java
+++ src/main/java/com/team766/robot/ArthurDoering/OI/CapstoneLights.java
@@ -1,12 +1,13 @@
 package com.team766.robot.ArthurDoering.OI;
 
 import static com.team766.framework.RulePersistence.*;
-
+import java.util.Set;
 import com.ctre.phoenix.led.*;
 import com.team766.framework.RuleGroup;
 import com.team766.hal.JoystickReader;
 import com.team766.hal.RobotProvider;
 import com.team766.robot.common.constants.ColorConstants;
+import com.team766.robot.common.constants.InputConstants;
 import com.team766.robot.common.mechanisms.LEDString;
 
 public class CapstoneLights extends RuleGroup {
@@ -14,30 +15,28 @@ public class CapstoneLights extends RuleGroup {
     private final LEDString.Segment segment = ledString.makeSegment(0, 512);
 
     public CapstoneLights() {
-        final JoystickReader buttonOne = RobotProvider.instance.getJoystick(1);
-        final JoystickReader buttonTwo = RobotProvider.instance.getJoystick(2);
-        final JoystickReader buttonThr = RobotProvider.instance.getJoystick(3);
+        final JoystickReader gamepad = RobotProvider.instance.getJoystick(1);
         addRule(
                 "Cone",
-                buttonOne.whenButton(1),
+                gamepad.whenButton(InputConstants.GAMEPAD_DPAD_LEFT),
                 ONCE_AND_HOLD,
-                ledString,
+                Set.of(ledString),
                 () -> {
                     segment.setColor(ColorConstants.YELLOW);
                 });
         addRule(
                 "Cube",
-                buttonTwo.whenButton(2),
+                gamepad.whenButton(InputConstants.GAMEPAD_DPAD_RIGHT),
                 ONCE_AND_HOLD,
-                ledString,
+                Set.of(ledString),
                 () -> {
                     segment.setColor(ColorConstants.PURPLE);
                 });
         addRule(
                 "Defense",
-                buttonThr.whenButton(3),
+                gamepad.whenButton(InputConstants.GAMEPAD_DPAD_DOWN),
                 ONCE_AND_HOLD,
-                ledString,
+                Set.of(ledString),
                 () -> {
                     Animation rainbowAnim = new RainbowAnimation();
                     segment.animate(rainbowAnim);
