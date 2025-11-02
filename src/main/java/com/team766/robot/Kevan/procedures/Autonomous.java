4b5d41ac92744a20109acabf47a7ddd274ae117a
kavusikevan-dot <kavusikevan@gmail.com>
2025-11-01 23:21:39 -0700

    rv

--- src/main/java/com/team766/robot/Kevan/procedures/Autonomous.java
+++ src/main/java/com/team766/robot/Kevan/procedures/Autonomous.java
@@ -5,24 +5,21 @@ import com.team766.framework.Procedure;
 import com.team766.robot.Kevan.mechanisms.Drive;
 import com.team766.robot.Kevan.mechanisms.Shooter;
 import com.team766.robot.Kevan.mechanisms.Intake;
+import com.team766.robot.Kevan.procedures.DriveProcedure;
+import com.team766.robot.Kevan.procedures.ShootProcedure;
+import com.team766.robot.Kevan.procedures.IntakeProcedure;
 
-public class Autonomous extends Procedure {
+public class Autonomous extends Procedure{
     private Drive drive;
     private Shooter shooter;
     private Intake intake;
 
     public Autonomous(Drive myDrive, Shooter myShooter, Intake myIntake) {
-        drive = reserve(myDrive);
-        shooter = reserve(myShooter);
-        intake = reserve(myIntake);
+        this.drive = reserve(myDrive);
+        this.shooter = reserve(myShooter);
+        this.intake = reserve(myIntake);
     }
 
     public void run(Context context) {
-        drive.move_straight(1);
-        context.waitForSeconds(2);
-        drive.move_straight(0);
-        shooter.SetShooterSpeed(0.5);
-        context.waitForSeconds(2);
-        shooter.SetShooterSpeed(0);
     }
 }
---
+++ src/main/java/com/team766/robot/Kevan/procedures/ShootProcedure.java
@@ -0,0 +1,25 @@
+package com.team766.robot.Kevan.procedures;
+
+import com.team766.framework.Context;
+import com.team766.framework.Procedure;
+import com.team766.robot.Kevan.mechanisms.Shooter;
+
+
+public class ShootProcedure extends Procedure {
+    
+    private Shooter shooter;
+    private double power;
+
+    public ShootProcedure(Shooter myShooter, double power) {
+        shooter = reserve(myShooter);
+        this.power = power;
+    }
+    public void run(Context context) {
+        shooter.SetShooterSpeed(power);
+        context.waitForSeconds(0.25);
+        shooter.SetTransferSpeed(1);
+        context.waitForSeconds(0.5);
+        shooter.SetTransferSpeed(0);
+        shooter.SetShooterSpeed(0);
+    }
+}
