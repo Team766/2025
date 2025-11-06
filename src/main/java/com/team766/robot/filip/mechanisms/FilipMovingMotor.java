6559401058d003f9106c2164d4aaea9630510fbf
filip-dob <filip.dobrzycki@gmail.com>
2025-11-05 18:55:14 -0800

    Refactor FilipMovingMotor to MovingMotor class

--- src/main/java/com/team766/robot/filip/mechanisms/FilipMovingMotor.java
+++ src/main/java/com/team766/robot/filip/mechanisms/FilipMovingMotor.java
@@ -1,25 +1,27 @@
-package com.team766.robot.filip.mechanisms;
-
-// importing previous functions, classes needed
+package com.team766.robot.Kevan.mechanisms;
 
 import com.team766.framework.MechanismWithStatus;
 import com.team766.framework.Status;
 import com.team766.hal.MotorController;
 import com.team766.hal.RobotProvider;
 
+public class MovingMotor extends MechanismWithStatus<MovingMotor.MovingMotorStatus> {
 
-public class FilipMovingMotor extends MechanismWithStatus<FilipMovingMotor.MovingMotorStatus> {
-  MotorController motor = RobotProvider.instance.getMotor("leftMotor");
-  public record MovingMotorStatus(double currentPosition) implements Status {}
- // creating a motor object
+    public MovingMotor() {}
 
-  public FilipMovingMotor() {}
-  protected MovingMotorStatus updateStatus(){
-    return new MovingMotorStatus(motor.getSensorPosition());
-  }
-  
-  public void moveSpeed(double speed) {
-    motor.set(speed);
-  }
+    MotorController motor = RobotProvider.instance.getMotor("mainMotor");
 
+    public void move(double motorPower) {
+        motor.set(motorPower);
+    }
+
+    public record MovingMotorStatus(double currentPosition) implements Status {}
+
+    public void stop() {
+        motor.set(0);
+    }
+
+    protected MovingMotorStatus updateStatus() {
+        return new MovingMotorStatus(motor.getSensorPosition());
+    }
 }
