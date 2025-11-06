9dab3029bbd37f832498a550d33baf921771b9c5
filip-dob <filip.dobrzycki@gmail.com>
2025-11-05 18:54:12 -0800

    Refactor Shooter class and update motor handling

--- src/main/java/com/team766/robot/filip/Shooter.java
+++ src/main/java/com/team766/robot/filip/Shooter.java
@@ -1,23 +1,28 @@
-package com.team766.robot.filip;
+package com.team766.robot.Kevan.mechanisms;
 
 import com.team766.framework.MechanismWithStatus;
 import com.team766.framework.Status;
-import com.team766.hal.RobotProvider;
 import com.team766.hal.MotorController;
+import com.team766.hal.RobotProvider;
 
-public class Shooter extends MechanismWithStatus<Shooter.ShooterStatus>{
-    MotorController shooter = RobotProvider.instance.getMotor("shooter");
-    public record ShooterStatus(double pos_shooter, double motorSpeed) implements Status {}
+public class Shooter extends MechanismWithStatus<Shooter.ShooterStatus> {
 
-    public Shooter(){
+    MotorController shooter_motor = RobotProvider.instance.getMotor("shooter_motor");
+    MotorController tranfer_motor = RobotProvider.instance.getMotor("transfer_motor");
 
+    public record ShooterStatus(double currentPosition) implements Status {}
+
+    public Shooter() {}
+
+    public void SetShooterSpeed(double motorPower) {
+        shooter_motor.set(motorPower);
     }
 
-    public void shootSpeed (double speed) {
-            shooter.set(speed);
+    public void SetTransferSpeed(double motorPower) {
+        tranfer_motor.set(motorPower);
     }
 
-    protected ShooterStatus updateStatus(){
-        return new ShooterStatus(shooter.getSensorPosition(), shooter.getSensorVelocity());
+    protected ShooterStatus updateStatus() {
+        return new ShooterStatus(0);
     }
 }
