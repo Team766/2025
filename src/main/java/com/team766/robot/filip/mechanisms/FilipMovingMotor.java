a519f89f2b910a66b5f435517f44ebb45e8b451f
filip-dob <filip.dobrzycki@gmail.com>
2025-11-05 18:56:50 -0800

    Rename MovingMotor class to FilipMovingMotor

--- src/main/java/com/team766/robot/filip/mechanisms/FilipMovingMotor.java
+++ src/main/java/com/team766/robot/filip/mechanisms/FilipMovingMotor.java
@@ -1,13 +1,13 @@
-package com.team766.robot.Kevan.mechanisms;
+package com.team766.robot.filip.mechanisms;
 
 import com.team766.framework.MechanismWithStatus;
 import com.team766.framework.Status;
 import com.team766.hal.MotorController;
 import com.team766.hal.RobotProvider;
 
-public class MovingMotor extends MechanismWithStatus<MovingMotor.MovingMotorStatus> {
+public class FilipMovingMotor extends MechanismWithStatus<MovingMotor.MovingMotorStatus> {
 
-    public MovingMotor() {}
+    public FilipMovingMotor() {}
 
     MotorController motor = RobotProvider.instance.getMotor("mainMotor");
 
