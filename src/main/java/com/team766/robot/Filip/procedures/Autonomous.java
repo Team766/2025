6d0c8aa3ea22148c3b675556cf28be57be4d7a71
filip-dob <filip.dobrzycki@gmail.com>
2025-11-12 17:49:40 -0800

    Refactor run method by removing unused procedures
    Removed multiple procedure calls from the run method.

--- src/main/java/com/team766/robot/Filip/procedures/Autonomous.java
+++ src/main/java/com/team766/robot/Filip/procedures/Autonomous.java
@@ -18,22 +18,6 @@ public class Autonomous extends Procedure {
     }
 
     public void run(Context context) {
-        context.runSync(new DriveProcedure(drive, 1.25));
-        context.runSync(new ShootProcedure(shooter, 0.8));
-        context.runSync(new TurnProcedure(drive, 0.25, 0.5));
-        context.runSync(new DriveProcedure(drive, 0.3125));
-        context.runSync(new IntakeProcedure(intake));
-        shooter.SetTransferSpeed(1);
-        context.waitForSeconds(0.5);
-        shooter.SetTransferSpeed(0);
-        context.runSync(new DriveProcedure(drive, 0.3125));
-        context.runSync(new IntakeProcedure(intake));
-        context.runSync(new TurnProcedure(drive, 0.5, 0.5));
-        context.runSync(new DriveProcedure(drive, 0.625));
-        context.runSync(new TurnProcedure(drive, 0.25, 0.5));
-        shooter.SetShooterSpeed(0.8);
-        context.waitForSeconds(0.25);
-        shooter.SetShooterSpeed(0);
-        context.runSync(new ShootProcedure(shooter, 0.8));
+
     }
 }
