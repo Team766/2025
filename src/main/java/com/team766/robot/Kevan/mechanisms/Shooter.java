9755cac658a24c95326fe2bf5b1fd55e7278e362
kavusikevan-dot <kavusikevan@gmail.com>
2025-11-17 20:23:07 -0800

    config part 2 goon

--- src/main/java/com/team766/robot/Kevan/mechanisms/Shooter.java
+++ src/main/java/com/team766/robot/Kevan/mechanisms/Shooter.java
@@ -7,8 +7,8 @@ import com.team766.hal.RobotProvider;
 
 public class Shooter extends MechanismWithStatus<Shooter.ShooterStatus> {
 
-    MotorController shooter_motor = RobotProvider.instance.getMotor("shooter.shooterMotor");
-    MotorController transfer_motor = RobotProvider.instance.getMotor("shooter.transferMotor");
+    MotorController shooter_motor = RobotProvider.instance.getMotor("shooter.shooter_motor");
+    MotorController transfer_motor = RobotProvider.instance.getMotor("shooter.transfer_motor");
 
     public record ShooterStatus(double pos_shooter, double pos_transfer) implements Status {}
 
