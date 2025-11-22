d6d7aec5d58635ce59e961338c23017cedce5c2a
DS <team766robotics@gmail.com>
2025-11-21 21:16:31 -0800

    current work

--- src/main/java/com/team766/robot/Jordan/mechanisms/MovingMotor.java
+++ src/main/java/com/team766/robot/Jordan/mechanisms/MovingMotor.java
@@ -1,35 +1,35 @@
 package com.team766.robot.Rookie_Training.mechanisms;
 
-import com.team776.framework.MechanismWithStatus;
 import com.team766.framework.Status;
 import com.team766.hal.MotorController;
 import com.team766.hal.RobotProvider;
+import com.team776.framework.MechanismWithStatus;
 
-public class MovingMotor extends MechanismWithStatus <MovingMotor.MovingMotorStatus> {
+public class MovingMotor extends MechanismWithStatus<MovingMotor.MovingMotorStatus> {
 
-    Public MovingMotor () {
+    public MovingMotor() {}
 
-    }
+    MotorController motor = RobotProvider.instance.getMotor("motor");
 
-    MotorController motor = RobotProvider.instance.getMotor(configName:"motor");
-
-    public record MovingMotorStatus (double currentPosition) implements Status {
-    }
+    public record MovingMotorStatus(double currentPosition) implements Status {}
 
     public void setMotorPower(double power) {
         motor.set(power);
     }
 
     protected MovingMotorStatus updateStatus() {
-            return MovingMotorStatus (MotorController.getSensorPosition());
+        return MovingMotorStatus(MotorController.getSensorPosition());
     }
 
-    //MotorController.getSensorPosition();
-    //MotorController.setPosition();
-    //MotorController.setCurrentLimit();
-    //MotorController.follow();
-    //MotorController.setInverted();
+    private MovingMotorStatus MovingMotorStatus(double sensorPosition) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'MovingMotorStatus'");
+    }
 
-
+    // MotorController.getSensorPosition();
+    // MotorController.setPosition();
+    // MotorController.setCurrentLimit();
+    // MotorController.follow();
+    // MotorController.setInverted();
 
 }
