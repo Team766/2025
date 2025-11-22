c146c8f48ef2095f5e2348917117db3e26eb3405
Raghav <raghav.sial21@gmail.com>
2025-11-22 07:52:50 -0800

    h

---
+++ src/main/java/com/team766/robot/mechanisms/MovingMotor.java
@@ -0,0 +1,13 @@
+import com.team766.simulator.elements.MotorController;
+
+public class MovingMotor {
+    public record MovingMotorStatus(double currentPosition) implements
+
+    Status {}
+    
+    protected MovingMotorStatus updateStatus(3)
+
+    public MovingMotor() {
+
+    }++
+}
