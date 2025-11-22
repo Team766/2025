3eee869ce59c9ab2f50ba8f029c01c7d86eb7b65
team766-laptop19 <admin@team766.com>
2025-11-22 08:18:07 -0800

    hashes

--- src/main/java/com/team766/robot/ArthurDoering/Robot.java
+++ src/main/java/com/team766/robot/ArthurDoering/Robot.java
@@ -6,9 +6,9 @@ import com.team766.hal.RobotConfigurator;
 import com.team766.robot.ArthurDoering.mechanisms.Drive;
 import com.team766.robot.ArthurDoering.mechanisms.Intake;
 import com.team766.robot.ArthurDoering.mechanisms.Shooter;
-import com.team766.robot.ArthurDoering.procedures.Autons.Autonomous;
+//import com.team766.robot.ArthurDoering.procedures.Autons.Autonomous;
 import com.team766.robot.ArthurDoering.procedures.Autons.EmptyAuton;
-import com.team766.robot.ArthurDoering.procedures.Autons.TestAuton;
+//import com.team766.robot.ArthurDoering.procedures.Autons.TestAuton;
 
 public class Robot implements RobotConfigurator {
     private Drive drive;
